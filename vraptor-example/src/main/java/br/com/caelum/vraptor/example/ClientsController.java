cfe6c74c45ef1f1c1f1a289d6deae5adfc44ea80
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2009-03-20 17:15:52 -0300

    base example using vraptor 3

---
+++ vraptor-example/src/main/java/br/com/caelum/vraptor/example/ClientsController.java
@@ -0,0 +1,14 @@
+package br.com.caelum.vraptor.example;
+
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+
+@Resource
+public class ClientsController {
+
+	private final Result result;
+
+	public ClientsController(Result result) {
+		this.result = result;
+	}
+}

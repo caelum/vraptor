b3f662943f775188c8a87850d35bd52fb43fbac7
U-User-PC\User <User@User-PC.(none)>
2009-03-22 13:54:51 -0300

    default method lookup algorithm test

--- vraptor-core/src/test/java/br/com/caelum/vraptor/resource/DefaultResourceAndMethodLookupTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/resource/DefaultResourceAndMethodLookupTest.java
@@ -1,17 +1,55 @@
 package br.com.caelum.vraptor.resource;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Before;
 import org.junit.Test;
+
+import br.com.caelum.vraptor.Path;
 
 public class DefaultResourceAndMethodLookupTest {
 
-	@Test
-	public void testFindsTheCorrectAnnotatedMethod() {
-		
+	private Mockery mockery;
+	private DefaultResourceAndMethodLookup lookuper;
+	private Resource resource;
+
+	@Before
+	public void setup() {
+		this.mockery = new Mockery();
+		this.resource = mockery.mock(Resource.class);
+		mockery.checking(new Expectations() {
+			{
+				one(resource).getType();
+				will(returnValue(Clients.class));
+			}
+		});
+		this.lookuper = new DefaultResourceAndMethodLookup(resource);
 	}
-	
+
+	@Test
+	public void testFindsTheCorrectAnnotatedMethod() throws SecurityException, NoSuchMethodException {
+		ResourceMethod method = lookuper.methodFor("/clients", "POST");
+		assertThat(method.getMethod(), is(equalTo(Clients.class.getMethod("add"))));
+		mockery.assertIsSatisfied();
+	}
+
 	@Test
 	public void testReturnsNullIfMethodNotFound() {
-		
+		ResourceMethod method = lookuper.methodFor("/projects", "POST");
+		assertThat(method, is(nullValue()));
+		mockery.assertIsSatisfied();
+	}
+
+	static class Clients {
+		@Path("/clients")
+		public void add() {
+
+		}
 	}
 
 }

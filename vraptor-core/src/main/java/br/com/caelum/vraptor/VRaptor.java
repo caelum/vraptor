b776c4e63ac5e29b1d54ef37121709a918a65930
Fabio Kung <fabio.kung@gmail.com>
2009-03-23 14:07:58 -0300

    only supports Servlet environments

--- vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
@@ -27,11 +27,17 @@ public class VRaptor implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res,
 			FilterChain chain) throws IOException, ServletException {
-		
-		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) res;
 
-		UrlToResourceTranslator translator = container
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        try {
+            request = (HttpServletRequest) req;
+            response = (HttpServletResponse) res;
+        } catch (ClassCastException e) {
+            throw new ServletException("VRaptor must be run inside a Servlet environment. Portlets and others aren't supported.", e);
+        }
+
+        UrlToResourceTranslator translator = container
 				.withA(UrlToResourceTranslator.class);
 		ResourceMethod method = translator.translate(request);
 		if (method == null) {

2a2f99050a330451939baac0736bfda0ad8cae3d
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2009-03-24 13:47:07 -0300

    removing wrong commited files

--- vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/pico/Base.java
+++
@@ -1,5 +0,0 @@
-package br.com.caelum.vraptor.ioc.pico;
-
-public class Base {
-
-}
--- vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/pico/Req.java
+++
@@ -1,9 +0,0 @@
-package br.com.caelum.vraptor.ioc.pico;
-
-public class Req {
-    
-    public Req(Base b) {
-        
-    }
-
-}

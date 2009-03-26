2a6dc7aaecfacbf568ba9cb4caf8d088345f17b1
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2009-03-26 11:50:50 -0300

    moving exceptions

---
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/InterceptionException.java
@@ -0,0 +1,9 @@
+package br.com.caelum.vraptor;
+
+public class InterceptionException extends VRaptorException {
+
+    public InterceptionException(Throwable e) {
+        super(e);
+    }
+
+}
---
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptorException.java
@@ -0,0 +1,9 @@
+package br.com.caelum.vraptor;
+
+public class VRaptorException extends Exception {
+
+    public VRaptorException(Throwable e) {
+        super(e);
+    }
+
+}

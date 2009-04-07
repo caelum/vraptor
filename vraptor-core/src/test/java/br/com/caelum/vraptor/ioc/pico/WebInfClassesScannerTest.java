b4cbb401585ecac52d926a58b2911262ead65dcf
Fabio Kung <fabio.kung@gmail.com>
2009-04-07 14:50:35 -0300

    missing interceptor registry mock

--- vraptor-core/src/test/java/br/com/caelum/vraptor/ioc/pico/WebInfClassesScannerTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/ioc/pico/WebInfClassesScannerTest.java
@@ -12,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.interceptor.InterceptorRegistry;
 import br.com.caelum.vraptor.resource.ResourceRegistry;
 
 public class WebInfClassesScannerTest {
@@ -29,20 +30,18 @@ public class WebInfClassesScannerTest {
 		final ServletContext context = mockery.mock(ServletContext.class);
 		final String myPath = "myPath";
 		final ResourceRegistry registry = mockery.mock(ResourceRegistry.class);
-		mockery.checking(new Expectations() {
+        final InterceptorRegistry interceptorRegistry = mockery.mock(InterceptorRegistry.class);
+        mockery.checking(new Expectations() {
 			{
 				one(context).getRealPath("");
 				will(returnValue(myPath));
-				one(scanner)
-						.scan(new File(new File(myPath), "WEB-INF/classes"));
+				one(scanner).scan(new File(new File(myPath), "WEB-INF/classes"));
 				will(returnValue(new ArrayList<Resource>()));
 				// TODO i forgot how to do it without the cast :)
-				one(registry)
-						.register(
-								(List<br.com.caelum.vraptor.resource.Resource>) with(a(List.class)));
+				one(registry).register((List<br.com.caelum.vraptor.resource.Resource>) with(a(List.class)));
 			}
 		});
-		new WebInfClassesScanner(context, scanner, registry).loadAll();
+		new WebInfClassesScanner(context, scanner, registry, interceptorRegistry).loadAll();
 		mockery.assertIsSatisfied();
 	}
 

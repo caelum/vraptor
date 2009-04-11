2a5a1999974d751be6253390c01f731349b06ffa
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2009-04-11 19:48:57 -0300

    fixing parameter provider tests

--- vraptor-core/src/test/java/br/com/caelum/vraptor/http/DefaultParameterNameProviderTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/http/DefaultParameterNameProviderTest.java
@@ -1,46 +1,54 @@
 package br.com.caelum.vraptor.http;
 
-import java.lang.reflect.ParameterizedType;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 import java.util.List;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
 public class DefaultParameterNameProviderTest {
     
-    private DefaultParameterNameProvider creator;
+    private DefaultParameterNameProvider provider;
 
     @Before
     public void setup() {
-        this.creator = new DefaultParameterNameProvider();
+        this.provider = new DefaultParameterNameProvider();
+    }
+    
+    class Field {
+        
+    }
+    interface Horse {
+        void runThrough(Field f);
+        void rest(int hours);
+        void setLeg(int[] length);
     }
     
     @Test
-    public void shouldNameObjectTypeAsItsSimpleName() {
-        MatcherAssert.assertThat(creator.extractName(DefaultParameterNameProviderTest.class), Matchers.is(Matchers.equalTo(DefaultParameterNameProviderTest.class.getSimpleName())));
+    public void shouldNameObjectTypeAsItsSimpleName() throws SecurityException, NoSuchMethodException {
+        assertThat(provider.parameterNamesFor(Horse.class.getMethod("runThrough", Field.class))[0], is(equalTo(DefaultParameterNameProviderTest.class.getSimpleName())));
     }
 
     @Test
-    public void shouldNamePrimitiveTypeAsItsSimpleName() {
-        MatcherAssert.assertThat(creator.extractName(int.class), Matchers.is(Matchers.equalTo(int.class.getSimpleName())));
+    public void shouldNamePrimitiveTypeAsItsSimpleName() throws SecurityException, NoSuchMethodException {
+        assertThat(provider.parameterNamesFor(Horse.class.getMethod("rest", int.class))[0], is(equalTo(int.class.getSimpleName())));
     }
 
     @Test
-    public void shouldNameArrayAsItsSimpleTypeName() {
-        MatcherAssert.assertThat(creator.extractName(int[].class), Matchers.is(Matchers.equalTo(int.class.getSimpleName())));
+    public void shouldNameArrayAsItsSimpleTypeName() throws SecurityException, NoSuchMethodException {
+        assertThat(provider.parameterNamesFor(Horse.class.getMethod("setLeg", int[].class))[0], is(equalTo(int.class.getSimpleName())));
     }
 
     @Test
     public void shouldNameGenericCollectionUsingOf() throws SecurityException, NoSuchMethodException {
-        ParameterizedType type = (ParameterizedType)Cat.class.getDeclaredMethod("fightWith", List.class).getGenericParameterTypes()[0];
-        MatcherAssert.assertThat(creator.extractName(type), Matchers.is(Matchers.equalTo("ListOfString")));
+        assertThat(provider.parameterNamesFor(Cat.class.getDeclaredMethod("fightWith", List.class))[0], is(equalTo("ListOfString")));
     }
     
     static public interface Cat {
         void fightWith(List<String> cats);
     }
-
 
 }

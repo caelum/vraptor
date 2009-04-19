e6f4b6d4874bf6fad7e4fbb754b41267ad54b32d
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2009-04-19 13:12:50 -0300

    uploaded file converter working

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/multipart/UploadedFileConverter.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/multipart/UploadedFileConverter.java
@@ -1,13 +1,27 @@
 package br.com.caelum.vraptor.interceptor.multipart;
 
+import javax.servlet.http.HttpServletRequest;
+
 import br.com.caelum.vraptor.Convert;
 import br.com.caelum.vraptor.Converter;
 
+/**
+ * A converter capable of setting UploadedFiles based on files parsed by the
+ * multipart interceptor.
+ * 
+ * @author Guilherme Silveira
+ */
 @Convert(UploadedFile.class)
-public class UploadedFileConverter implements Converter<UploadedFile>{
+public class UploadedFileConverter implements Converter<UploadedFile> {
+    
+    private final HttpServletRequest request;
+
+    public UploadedFileConverter(HttpServletRequest request) {
+        this.request = request;
+    }
 
     public UploadedFile convert(String value, Class<? extends UploadedFile> type) {
-        return null;
+        return (UploadedFile) request.getAttribute(value);
     }
 
 }

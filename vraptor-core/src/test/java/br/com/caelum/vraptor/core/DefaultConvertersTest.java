04ddef4069e548866600989fd16b686f3212da64
Fabio Kung <fabio.kung@gmail.com>
2009-04-22 15:23:58 -0300

    testing that all default converters are registered plus adding support for mocking concrete classes

--- vraptor-core/src/test/java/br/com/caelum/vraptor/core/DefaultConvertersTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/core/DefaultConvertersTest.java
@@ -1,18 +1,43 @@
 package br.com.caelum.vraptor.core;
 
+import br.com.caelum.vraptor.ComponentRegistry;
+import br.com.caelum.vraptor.Convert;
+import br.com.caelum.vraptor.Converter;
+import br.com.caelum.vraptor.VRaptorMockery;
+import br.com.caelum.vraptor.converter.BooleanConverter;
+import br.com.caelum.vraptor.converter.ByteConverter;
+import br.com.caelum.vraptor.converter.DoubleConverter;
+import br.com.caelum.vraptor.converter.EnumConverter;
+import br.com.caelum.vraptor.converter.FloatConverter;
+import br.com.caelum.vraptor.converter.IntegerConverter;
+import br.com.caelum.vraptor.converter.LocaleBasedCalendarConverter;
+import br.com.caelum.vraptor.converter.LocaleBasedDateConverter;
+import br.com.caelum.vraptor.converter.LongConverter;
+import br.com.caelum.vraptor.converter.PrimitiveBooleanConverter;
+import br.com.caelum.vraptor.converter.PrimitiveByteConverter;
+import br.com.caelum.vraptor.converter.PrimitiveDoubleConverter;
+import br.com.caelum.vraptor.converter.PrimitiveFloatConverter;
+import br.com.caelum.vraptor.converter.PrimitiveIntConverter;
+import br.com.caelum.vraptor.converter.PrimitiveLongConverter;
+import br.com.caelum.vraptor.converter.PrimitiveShortConverter;
+import br.com.caelum.vraptor.converter.ShortConverter;
+import br.com.caelum.vraptor.interceptor.multipart.UploadedFileConverter;
+import br.com.caelum.vraptor.interceptor.multipart.UploadedFile;
+import br.com.caelum.vraptor.ioc.Container;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.hamcrest.Matchers.typeCompatibleWith;
-
+import static org.hamcrest.Matchers.instanceOf;
+import org.hamcrest.Matchers;
 import org.jmock.Expectations;
 import org.junit.Before;
 import org.junit.Test;
 
-import br.com.caelum.vraptor.Convert;
-import br.com.caelum.vraptor.Converter;
-import br.com.caelum.vraptor.ComponentRegistry;
-import br.com.caelum.vraptor.VRaptorMockery;
-import br.com.caelum.vraptor.ioc.Container;
+import java.util.HashMap;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
 
 public class DefaultConvertersTest {
 
@@ -23,15 +48,59 @@ public class DefaultConvertersTest {
 
     @Before
     public void setup() {
-        this.mockery = new VRaptorMockery();
+        this.mockery = new VRaptorMockery(true);
         this.container = mockery.mock(Container.class);
         this.componentRegistry = mockery.mock(ComponentRegistry.class);
         mockery.checking(new Expectations() {
             {
-                allowing(componentRegistry).register((Class)with(an(Class.class)));
+                allowing(componentRegistry).register((Class) with(an(Class.class)));
             }
         });
         this.converters = new DefaultConverters(componentRegistry);
+    }
+
+    @Test
+    public void shouldRegisterConvertersForAllDefaultTypes() {
+        final HashMap<Class, Class<? extends Converter>> EXPECTED_CONVERTERS = new HashMap<Class, Class<? extends Converter>>() {
+            {
+                put(int.class, PrimitiveIntConverter.class);
+                put(long.class, PrimitiveLongConverter.class);
+                put(short.class, PrimitiveShortConverter.class);
+                put(byte.class, PrimitiveByteConverter.class);
+                put(double.class, PrimitiveDoubleConverter.class);
+                put(float.class, PrimitiveFloatConverter.class);
+                put(boolean.class, PrimitiveBooleanConverter.class);
+                put(Integer.class, IntegerConverter.class);
+                put(Long.class, LongConverter.class);
+                put(Short.class, ShortConverter.class);
+                put(Byte.class, ByteConverter.class);
+                put(Double.class, DoubleConverter.class);
+                put(Float.class, FloatConverter.class);
+                put(Boolean.class, BooleanConverter.class);
+                put(Calendar.class, LocaleBasedCalendarConverter.class);
+                put(Date.class, LocaleBasedDateConverter.class);
+                put(Enum.class, EnumConverter.class);
+                put(UploadedFile.class, UploadedFileConverter.class);
+            }
+        };
+
+        mockery.checking(new Expectations() {
+            {
+                for (Class<? extends Converter> converterType : EXPECTED_CONVERTERS.values()) {
+                    Converter expected = mockery.mock(converterType);
+                    one(componentRegistry).register(converterType);
+                    one(container).instanceFor(converterType);
+                    will(returnValue(expected));
+                }
+            }
+        });
+
+        for (Map.Entry<Class, Class<? extends Converter>> entry : EXPECTED_CONVERTERS.entrySet()) {
+            Class typeFor = entry.getKey();
+            Class<? extends Converter> converterType = entry.getValue();
+            Converter converter = converters.to(typeFor, container);
+            assertThat(converter, is(instanceOf(converterType)));
+        }
     }
 
     @Test(expected = IllegalArgumentException.class)

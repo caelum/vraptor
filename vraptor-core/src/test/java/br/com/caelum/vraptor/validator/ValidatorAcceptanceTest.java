b830e0ceba418d68128af506ddbd3d59b85b191d
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2009-04-22 12:10:56 -0300

    fixing compilation issues and vatest

--- vraptor-core/src/test/java/br/com/caelum/vraptor/validator/ValidatorAcceptanceTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/validator/ValidatorAcceptanceTest.java
@@ -3,7 +3,17 @@ package br.com.caelum.vraptor.validator;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import br.com.caelum.vraptor.view.jsp.PageResult;
 
 public class ValidatorAcceptanceTest {
 
@@ -11,20 +21,42 @@ public class ValidatorAcceptanceTest {
         private Long id;
     }
 
-    @Test(expected = ValidationError.class)
-    public void cheksThatValidationWorks() {
-        DefaultValidator validator = new DefaultValidator();
+    private Mockery mockery;
+    private PageResult result;
+
+    @Before
+    public void setup() {
+        this.mockery = new Mockery();
+        this.result = mockery.mock(PageResult.class, "result");
+    }
+
+    @Test
+    public void cheksThatValidationWorks() throws ServletException, IOException {
+        DefaultValidator validator = new DefaultValidator(result);
         final Student guilherme = new Student();
-        validator.checking(new Validations() {
+        mockery.checking(new Expectations() {
             {
-                that(guilherme.id, is(notNullValue()));
+                one(result).include((String) with(an(String.class)),with(an(Object.class)) );
+                one(result).forward("invalid");
             }
         });
+        try {
+            validator.checking(new Validations() {
+                {
+                    that(guilherme.id, is(notNullValue()));
+                }
+            });
+            Assert.fail();
+        } catch (ValidationError e) {
+            // should be here to check mockery values
+            // DO NOT use (expected=...)
+            mockery.assertIsSatisfied();
+        }
     }
 
     @Test
     public void validDataDoesntThrowException() {
-        DefaultValidator validator = new DefaultValidator();
+        DefaultValidator validator = new DefaultValidator(result);
         final Student guilherme = new Student();
         guilherme.id = 15L;
         validator.checking(new Validations() {
@@ -33,6 +65,7 @@ public class ValidatorAcceptanceTest {
                 that(guilherme.id, is(notNullValue()));
             }
         });
+        mockery.assertIsSatisfied();
     }
 
 }

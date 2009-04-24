13d43d213e3683b6de09288a4f562fcfe218d8f7
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2009-04-24 12:39:47 -0300

    compilation issue fixed

--- vraptor-core/src/test/java/br/com/caelum/vraptor/vraptor2/ValidatorInterceptorTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/vraptor2/ValidatorInterceptorTest.java
@@ -28,6 +28,7 @@ public class ValidatorInterceptorTest {
     private ValidationErrors errors;
     private Outjecter outjecter;
     private OutjectionInterceptor interceptor;
+    private ComponentInfoProvider info;
 
     @Before
     public void setup() {
@@ -36,7 +37,13 @@ public class ValidatorInterceptorTest {
         this.provider = mockery.mock(ParametersProvider.class);
         this.errors = mockery.mock(ValidationErrors.class);
         this.outjecter = mockery.mock(Outjecter.class);
-        this.interceptor = new OutjectionInterceptor(outjecter);
+        this.info = mockery.mock(ComponentInfoProvider.class);
+        mockery.checking(new Expectations() {
+            {
+                one(info).getOutjecter(); will(returnValue(outjecter));
+            }
+        });
+        this.interceptor = new OutjectionInterceptor(info);
         this.validator = new ValidatorInterceptor(this.provider, this.result, errors, interceptor);
         this.stack = mockery.mock(InterceptorStack.class);
     }
@@ -58,17 +65,19 @@ public class ValidatorInterceptorTest {
         validator.intercept(stack, method, resourceInstance);
         mockery.assertIsSatisfied();
     }
-    
+
     @org.vraptor.annotations.Component
     interface MyComponent {
         void noValidation();
+
         void withValidation();
+
         void validateWithValidation(ValidationErrors errors);
     }
 
-
     @Test
-    public void doestNothingIfValidationMethodNotFound() throws NoSuchMethodException, InterceptionException, IOException {
+    public void doestNothingIfValidationMethodNotFound() throws NoSuchMethodException, InterceptionException,
+            IOException {
         final MyComponent resourceInstance = mockery.mock(MyComponent.class);
         final ResourceMethod method = mockery.methodFor(MyComponent.class, "noValidation");
         mockery.checking(new Expectations() {
@@ -81,25 +90,30 @@ public class ValidatorInterceptorTest {
     }
 
     @Test
-    public void forwardToValidationPageWithErrorsIfSomeFound() throws NoSuchMethodException, InterceptionException, IOException, ServletException {
+    public void forwardToValidationPageWithErrorsIfSomeFound() throws NoSuchMethodException, InterceptionException,
+            IOException, ServletException {
         final Message message = new Message("", "");
         final MyComponent resourceInstance = new MyComponent() {
             public void validateWithValidation(ValidationErrors errors) {
                 errors.add(message);
             }
+
             public void noValidation() {
             }
+
             public void withValidation() {
             }
         };
         final ResourceMethod method = mockery.methodFor(MyComponent.class, "withValidation");
         mockery.checking(new Expectations() {
             {
-                one(provider).getParametersFor(method); will(returnValue(new Object[0]));
+                one(provider).getParametersFor(method);
+                will(returnValue(new Object[0]));
                 one(result).include(with(equal("errors")), with(an(ValidationErrors.class)));
                 one(result).forward("invalid");
                 one(errors).add(message);
-                one(errors).size(); will(returnValue(1));
+                one(errors).size();
+                will(returnValue(1));
             }
         });
         validator.intercept(stack, method, resourceInstance);
@@ -107,21 +121,26 @@ public class ValidatorInterceptorTest {
     }
 
     @Test
-    public void doesNothingIfValidationMethodExistsButNoErrorsOccur() throws NoSuchMethodException, InterceptionException, IOException {
+    public void doesNothingIfValidationMethodExistsButNoErrorsOccur() throws NoSuchMethodException,
+            InterceptionException, IOException {
         final MyComponent resourceInstance = new MyComponent() {
             public void validateWithValidation(ValidationErrors errors) {
             }
+
             public void noValidation() {
             }
+
             public void withValidation() {
             }
         };
         final ResourceMethod method = mockery.methodFor(MyComponent.class, "withValidation");
         mockery.checking(new Expectations() {
             {
-                one(provider).getParametersFor(method); will(returnValue(new Object[0]));
+                one(provider).getParametersFor(method);
+                will(returnValue(new Object[0]));
                 one(stack).next(method, resourceInstance);
-                one(errors).size(); will(returnValue(0));
+                one(errors).size();
+                will(returnValue(0));
             }
         });
         validator.intercept(stack, method, resourceInstance);

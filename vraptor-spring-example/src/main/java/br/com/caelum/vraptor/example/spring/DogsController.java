0b8a7f62231266bc4c509184fc2a36d333d95681
Fabio Kung <fabio.kung@gmail.com>
2009-04-28 16:14:30 -0300

    removing not required @In annotation

--- vraptor-spring-example/src/main/java/br/com/caelum/vraptor/example/spring/DogsController.java
+++ vraptor-spring-example/src/main/java/br/com/caelum/vraptor/example/spring/DogsController.java
@@ -1,10 +1,10 @@
 package br.com.caelum.vraptor.example.spring;
 
-import br.com.caelum.vraptor.In;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.view.Results;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 
@@ -14,7 +14,7 @@ import java.util.Arrays;
 @Resource
 public class DogsController {
 
-    @In
+    @Autowired
     private Result result;
 
     @Path("/dogs")

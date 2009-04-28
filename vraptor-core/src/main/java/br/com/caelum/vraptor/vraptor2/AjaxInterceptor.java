d39de3d0fb0dfa46417c70711fd4066e50ec5175
Guilherme Silveira <guilherme@paradise.local>
2009-04-28 15:18:57 -0300

    fix for viewless json request

--- vraptor-core/src/main/java/br/com/caelum/vraptor/vraptor2/AjaxInterceptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/vraptor2/AjaxInterceptor.java
@@ -1,3 +1,30 @@
+/***
+ * 
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. Neither the name of the
+ * copyright holders nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written
+ * permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package br.com.caelum.vraptor.vraptor2;
 
 import java.io.IOException;
@@ -15,54 +42,56 @@ import br.com.caelum.vraptor.resource.ResourceMethod;
 import br.com.caelum.vraptor.vraptor2.outject.JsonOutjecter;
 
 /**
- * VRaptor2 based ajax interceptor.
- *
+ * VRaptor2 based ajax interceptor.<br>
+ * Only outjects data if its not a viewless method.
+ * 
  * @author Guilherme Silveira
  */
 public class AjaxInterceptor implements Interceptor {
 
-    private static final String UTF8 = "UTF-8";
+	private static final String UTF8 = "UTF-8";
 
-    private final ComponentInfoProvider info;
+	private final ComponentInfoProvider info;
 
-    private final HttpServletResponse response;
+	private final HttpServletResponse response;
 
-    public AjaxInterceptor(HttpServletResponse response, ComponentInfoProvider info) {
-        this.response = response;
-        this.info = info;
+	public AjaxInterceptor(HttpServletResponse response, ComponentInfoProvider info) {
+		this.response = response;
+		this.info = info;
 
-    }
+	}
 
-    public boolean accepts(ResourceMethod method) {
-        // TODO this is not invoked as automatically loaded thorugh
-        // RequestExecution
-        // it should be included on the ExtractorList so would not be invoked?
-        return info.isAjax();
-    }
+	public boolean accepts(ResourceMethod method) {
+		// TODO this is not invoked as automatically loaded thorugh
+		// RequestExecution
+		// it should be included on the ExtractorList so would not be invoked?
+		return info.isAjax();
+	}
 
-    public void intercept(InterceptorStack stack, ResourceMethod method, Object resourceInstance) throws InterceptionException {
-        if (info.isAjax()) {
-            if (!method.getMethod().isAnnotationPresent(Remotable.class)) {
-                throw new InterceptionException("Unable to make an ajax result in a non-remotable method.");
-            }
-            int depth = method.getMethod().getAnnotation(Remotable.class).depth();
-            JsonOutjecter outjecter = (JsonOutjecter) info.getOutjecter();
-            CharSequence output = new JSONSerializer(depth).serialize(outjecter.contents());
-            response.setCharacterEncoding(UTF8);
-            response.setContentType("application/json");
+	public void intercept(InterceptorStack stack, ResourceMethod method, Object resourceInstance)
+			throws InterceptionException {
+		if (info.shouldShowView(method) && info.isAjax()) {
+			if (!method.getMethod().isAnnotationPresent(Remotable.class)) {
+				throw new InterceptionException("Unable to make an ajax result in a non-remotable method.");
+			}
+			int depth = method.getMethod().getAnnotation(Remotable.class).depth();
+			JsonOutjecter outjecter = (JsonOutjecter) info.getOutjecter();
+			CharSequence output = new JSONSerializer(depth).serialize(outjecter.contents());
+			response.setCharacterEncoding(UTF8);
+			response.setContentType("application/json");
 
-            PrintWriter writer = null;
-            try {
-                writer = response.getWriter();
-                writer.append(output);
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                throw new InterceptionException(e);
-            }
-        } else {
-            stack.next(method, resourceInstance);
-        }
-    }
+			PrintWriter writer = null;
+			try {
+				writer = response.getWriter();
+				writer.append(output);
+				writer.flush();
+				writer.close();
+			} catch (IOException e) {
+				throw new InterceptionException(e);
+			}
+		} else {
+			stack.next(method, resourceInstance);
+		}
+	}
 
 }
--- vraptor-core/src/test/java/br/com/caelum/vraptor/vraptor2/AjaxInterceptorTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/vraptor2/AjaxInterceptorTest.java
@@ -1,3 +1,30 @@
+/***
+ * 
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. Neither the name of the
+ * copyright holders nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written
+ * permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package br.com.caelum.vraptor.vraptor2;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -48,14 +75,31 @@ public class AjaxInterceptorTest {
 
     @Test
     public void invokesNextIfNonAjax() throws InterceptionException, IOException {
+    	final ResourceMethod method = mockery.mock(ResourceMethod.class);
         mockery.checking(new Expectations() {
             {
+                one(info).shouldShowView(method);
+                will(returnValue(true));
                 one(info).isAjax();
                 will(returnValue(false));
-                one(stack).next(null, null);
+                one(stack).next(method, null);
             }
         });
-        interceptor.intercept(stack, null, null);
+        interceptor.intercept(stack, method, null);
+        mockery.assertIsSatisfied();
+    }
+
+    @Test
+    public void invokesNextIfViewless() throws InterceptionException, IOException {
+    	final ResourceMethod method = mockery.mock(ResourceMethod.class);
+        mockery.checking(new Expectations() {
+            {
+                one(info).shouldShowView(method);
+                will(returnValue(false));
+                one(stack).next(method, null);
+            }
+        });
+        interceptor.intercept(stack, method, null);
         mockery.assertIsSatisfied();
     }
 
@@ -73,9 +117,11 @@ public class AjaxInterceptorTest {
         outjecter.include("author", "Guilherme");
         StringWriter content = new StringWriter();
         final PrintWriter writer = new PrintWriter(content);
-        ResourceMethod method = mockery.methodFor(MyComponent.class, "ajaxed");
+        final ResourceMethod method = mockery.methodFor(MyComponent.class, "ajaxed");
         mockery.checking(new Expectations() {
             {
+                one(info).shouldShowView(method);
+                will(returnValue(true));
                 one(info).isAjax();
                 will(returnValue(true));
                 one(response).setContentType("application/json");
@@ -92,9 +138,11 @@ public class AjaxInterceptorTest {
     @Test
     public void complainsIfTryingToAjaxANonRemotableMethod() throws IOException, NoSuchMethodException {
         outjecter.include("author", "Guilherme");
-        ResourceMethod method = mockery.methodFor(MyComponent.class, "nonAjaxed");
+        final ResourceMethod method = mockery.methodFor(MyComponent.class, "nonAjaxed");
         mockery.checking(new Expectations() {
             {
+                one(info).shouldShowView(method);
+                will(returnValue(true));
                 one(info).isAjax();
                 will(returnValue(true));
             }

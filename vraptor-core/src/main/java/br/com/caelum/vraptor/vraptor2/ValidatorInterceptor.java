be6adc28110289d9527f9d3eea250a853e5c7357
Guilherme Silveira <guilherme@paradise.local>
2009-04-29 12:18:20 -0300

    vlidaton interceptor with correct message format on errors

--- vraptor-core/src/main/java/br/com/caelum/vraptor/vraptor2/ValidatorInterceptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/vraptor2/ValidatorInterceptor.java
@@ -1,7 +1,35 @@
+/***
+ * 
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. Neither the name of the
+ * copyright holders nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written
+ * permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package br.com.caelum.vraptor.vraptor2;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -9,6 +37,8 @@ import java.util.ResourceBundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vraptor.i18n.FixedMessage;
+import org.vraptor.i18n.Message;
+import org.vraptor.validator.BasicValidationErrors;
 import org.vraptor.validator.ValidationErrors;
 
 import br.com.caelum.vraptor.InterceptionException;
@@ -47,11 +77,12 @@ public class ValidatorInterceptor implements Interceptor {
             Class<?> type = method.getResource().getType();
             Method validationMethod = getValidationFor(method.getMethod(), type);
             if (validationMethod != null) {
-            	List<ValidationMessage> messages= new ArrayList<ValidationMessage>();
+            	List<ValidationMessage> convertionErrors= new ArrayList<ValidationMessage>();
                 ResourceBundle bundle = localization.getBundle();
-				Object[] parameters = provider.getParametersFor(method, messages, bundle);
+				Object[] parameters = provider.getParametersFor(method, convertionErrors, bundle);
                 Object[] validationParameters = new Object[parameters.length + 1];
-                validationParameters[0] = errors;
+                BasicValidationErrors newErrors = new BasicValidationErrors();
+                validationParameters[0] = newErrors;
                 for (int i = 0; i < parameters.length; i++) {
                     validationParameters[i + 1] = parameters[i];
                 }
@@ -64,8 +95,20 @@ public class ValidatorInterceptor implements Interceptor {
                 } catch (InvocationTargetException e) {
                     throw new InterceptionException("Unable to validate.", e.getCause());
                 }
-                for(ValidationMessage msg : messages) {
-                	errors.add(new FixedMessage(msg.getCategory(), bundle.getString(msg.getMessage()), msg.getCategory()));
+                for(ValidationMessage msg : convertionErrors) {
+                	errors.add(new FixedMessage(msg.getCategory(), msg.getMessage(), msg.getCategory()));
+                }
+                for(org.vraptor.i18n.ValidationMessage msg : newErrors) {
+                	if(msg instanceof FixedMessage) {
+                		errors.add(msg);
+                	} else if (msg instanceof Message){
+                		Message m = (Message) msg;
+                		String content = bundle.getString(m.getKey());
+                		content = MessageFormat.format(content, m.getParameters());
+                		errors.add(new FixedMessage(msg.getPath(), content, msg.getCategory()));
+                	} else {
+                		throw new IllegalArgumentException("Unsupported validation message type: " + msg.getClass().getName());
+                	}
                 }
             }
             if (errors.size() != 0) {

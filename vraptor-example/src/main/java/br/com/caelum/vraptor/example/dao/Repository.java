4ec906d5a4f9f71d28bad578bda155fa3869c6a3
Fabio Kung <fabio.kung@gmail.com>
2009-05-12 18:11:30 -0300

    fixing compilation problems

--- vraptor-example/src/main/java/br/com/caelum/vraptor/example/dao/Repository.java
+++ vraptor-example/src/main/java/br/com/caelum/vraptor/example/dao/Repository.java
@@ -1,7 +1,7 @@
 /***
- * 
+ *
  * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 1. Redistributions of source code must retain the above copyright notice,
@@ -12,7 +12,7 @@
  * copyright holders nor the names of its contributors may be used to endorse or
  * promote products derived from this software without specific prior written
  * permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,18 +27,14 @@
  */
 package br.com.caelum.vraptor.example.dao;
 
-import java.util.Collection;
-
-<<<<<<< HEAD:vraptor-example/src/main/java/br/com/caelum/vraptor/example/dao/Repository.java
 import br.com.caelum.vraptor.example.Client;
-=======
-import br.com.caelum.vraptor.http.route.Route;
->>>>>>> rule --> route:vraptor-core/src/main/java/br/com/caelum/vraptor/resource/ResourceParserRoutesCreator.java
+
+import java.util.Collection;
 
 /**
  * A simple interface for stored data.
- * @author guilherme silveira
  *
+ * @author guilherme silveira
  */
 public interface Repository {
 
@@ -46,12 +42,8 @@ public interface Repository {
 
     void add(Client c);
 
-	void remove(Client client);
+    void remove(Client client);
 
-<<<<<<< HEAD:vraptor-example/src/main/java/br/com/caelum/vraptor/example/dao/Repository.java
-	Client find(Long id);
-=======
-	List<Route> rulesFor(Resource resource);
->>>>>>> rule --> route:vraptor-core/src/main/java/br/com/caelum/vraptor/resource/ResourceParserRoutesCreator.java
+    Client find(Long id);
 
 }

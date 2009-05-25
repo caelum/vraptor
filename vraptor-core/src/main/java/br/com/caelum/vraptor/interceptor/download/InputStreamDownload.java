892e5981dec83ce2559f23391181ea44a56407e2
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2009-05-25 11:33:04 -0300

    simple refactor

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/download/InputStreamDownload.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/download/InputStreamDownload.java
@@ -28,15 +28,34 @@
 package br.com.caelum.vraptor.interceptor.download;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Generic downloader.
+ * Handles download by reading from a input stream byte by byte.
  * @author filipesabella
  */
-public interface Download {
+public class InputStreamDownload implements Download {
+	private InputStream input;
+	private FileDetails details;
 
-	public abstract void write(HttpServletResponse response) throws IOException;
+	public InputStreamDownload(InputStream input, String contentType, String fileName) {
+		this(input, contentType, fileName, false);
+	}
+	public InputStreamDownload(InputStream input, String contentType, String fileName, boolean doDownload) {
+		this.input = input;
+		this.details = new FileDetails(contentType, fileName, doDownload);
+	}
 
+	public void write(HttpServletResponse response) throws IOException {
+		details.write(response);
+
+		OutputStream output = response.getOutputStream();
+		int read = 0;
+		while((read = input.read()) > -1) {
+			output.write(read);
+		}
+	}
 }

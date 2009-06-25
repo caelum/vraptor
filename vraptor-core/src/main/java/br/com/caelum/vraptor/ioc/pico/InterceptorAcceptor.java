a6387b6da350446fcb7b035d05a54148154ea5c7
Paulo Silveira <paulo.silveira@caelum.com.br>
2009-06-25 17:50:19 -0300

    logging correctly which class has the worng annotation

--- vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/pico/InterceptorAcceptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/pico/InterceptorAcceptor.java
@@ -13,37 +13,50 @@ import br.com.caelum.vraptor.interceptor.InterceptorSequence;
 
 public class InterceptorAcceptor implements Acceptor {
 
-    private static final Logger logger = LoggerFactory.getLogger(InterceptorAcceptor.class);
-    private final List<Class<? extends Interceptor>> interceptors;
+	private static final Logger logger = LoggerFactory
+			.getLogger(InterceptorAcceptor.class);
+	private final List<Class<? extends Interceptor>> interceptors;
 
-    public InterceptorAcceptor(List<Class<? extends Interceptor>> interceptors) {
-        this.interceptors = interceptors;
-    }
+	public InterceptorAcceptor(List<Class<? extends Interceptor>> interceptors) {
+		this.interceptors = interceptors;
+	}
 
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public void analyze(Class<?> type) {
-        if (type.isAnnotationPresent(Intercepts.class)) {
-            if (Interceptor.class.isAssignableFrom(type)) {
-                logger.debug("Found interceptor for " + type);
-                interceptors.add((Class<Interceptor>) type);
-            } else if (InterceptorSequence.class.isAssignableFrom(type)) {
-                logger.debug("Found interceptor sequence for " + type);
-                parseSequence(type);
-            } else {
-                logger.error("Annotation " + Intercepts.class + " found but this is neither an Interceptor nor an InterceptorSequence. Ignoring");
-            }
-        }
-    }
+		if (type.isAnnotationPresent(Intercepts.class)) {
+			if (Interceptor.class.isAssignableFrom(type)) {
+				logger.debug("Found interceptor for " + type);
+				interceptors.add((Class<Interceptor>) type);
+			} else if (InterceptorSequence.class.isAssignableFrom(type)) {
+				logger.debug("Found interceptor sequence for " + type);
+				parseSequence(type);
+			} else {
+				logger
+						.error("Annotation "
+								+ Intercepts.class
+								+ " found in "
+								+ type
+								+ " but this is neither an Interceptor nor an InterceptorSequence. Ignoring");
+			}
+		}
+	}
 
-    private void parseSequence(Class<?> type) {
-        try {
-            InterceptorSequence sequence = InterceptorSequence.class.cast(type.getConstructor().newInstance());
-            interceptors.addAll(Arrays.asList(sequence.getSequence()));
-        } catch (InvocationTargetException e) {
-            logger.error("Problem ocurred while instantiating an interceptor sequence",e.getCause());
-        } catch (Exception e) {
-            logger.error("Problem ocurred while instantiating an interceptor sequence",e);
-        }
-    }
+	private void parseSequence(Class<?> type) {
+		try {
+			InterceptorSequence sequence = InterceptorSequence.class.cast(type
+					.getConstructor().newInstance());
+			interceptors.addAll(Arrays.asList(sequence.getSequence()));
+		} catch (InvocationTargetException e) {
+			logger
+					.error(
+							"Problem ocurred while instantiating an interceptor sequence",
+							e.getCause());
+		} catch (Exception e) {
+			logger
+					.error(
+							"Problem ocurred while instantiating an interceptor sequence",
+							e);
+		}
+	}
 
 }

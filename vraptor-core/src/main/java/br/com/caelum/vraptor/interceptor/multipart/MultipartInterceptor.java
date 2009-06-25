c7e141cc0dca64b6919895e1c41f1ac73ede5771
Paulo Silveira <paulo.silveira@caelum.com.br>
2009-06-25 17:37:35 -0300

    only creating temp directory if the message is Multipart (it was being created every single request)

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/multipart/MultipartInterceptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/multipart/MultipartInterceptor.java
@@ -26,77 +26,92 @@ import br.com.caelum.vraptor.resource.ResourceMethod;
  * An interceptor which handles multipart requests.<br>
  * Provided parameters are injected through RequestParameters.set and uploaded
  * files are made available through
- *
+ * 
  * @author Guilherme Silveira
  */
 @RequestScoped
 public class MultipartInterceptor implements Interceptor {
 
-    private static final Logger logger = LoggerFactory.getLogger(MultipartInterceptor.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(MultipartInterceptor.class);
 
-    private final File temporaryDirectory;
+	private final long sizeLimit;
 
-    private final long sizeLimit;
+	private final HttpServletRequest request;
 
-    private final HttpServletRequest request;
+	private final MutableRequest parameters;
 
-    private final MutableRequest parameters;
+	private final MultipartConfig config;
 
-    public MultipartInterceptor(HttpServletRequest request, MutableRequest parameters, MultipartConfig config) throws IOException {
-        this.request = request;
-        this.parameters = parameters;
-        this.sizeLimit = config.getSizeLimit();
-        this.temporaryDirectory = config.getDirectory();
-    }
+	public MultipartInterceptor(HttpServletRequest request,
+			MutableRequest parameters, MultipartConfig config)
+			throws IOException {
+		this.request = request;
+		this.parameters = parameters;
+		this.sizeLimit = config.getSizeLimit();
+		this.config = config;
+	}
 
-    @SuppressWarnings("unchecked")
-    public void intercept(InterceptorStack stack, ResourceMethod method, Object instance) throws InterceptionException {
-    	// TODO ugly, just for now until next release
-		if(!accepts(method)) {
+	@SuppressWarnings("unchecked")
+	public void intercept(InterceptorStack stack, ResourceMethod method,
+			Object instance) throws InterceptionException {
+		// TODO ugly, just for now until next release
+		if (!accepts(method)) {
 			stack.next(method, instance);
-		    return;
+			return;
 		}
 
-        logger.debug("Trying to parse multipart request.");
+		logger.debug("Trying to parse multipart request.");
 
-        DiskFileItemFactory factory = createFactoryForDiskBasedFileItems();
+		File temporaryDirectory = config.getDirectory();
+		DiskFileItemFactory factory = createFactoryForDiskBasedFileItems(temporaryDirectory);
 
-        ServletFileUpload fileUploadHandler = new ServletFileUpload(factory);
+		ServletFileUpload fileUploadHandler = new ServletFileUpload(factory);
 
-        fileUploadHandler.setSizeMax(sizeLimit);
+		fileUploadHandler.setSizeMax(sizeLimit);
 
-        List<FileItem> fileItems;
-        try {
-            fileItems = fileUploadHandler.parseRequest(request);
-        } catch (FileUploadException e) {
-            logger.warn("There was some problem parsing this multipart request, "
-                    + "or someone is not sending a RFC1867 compatible multipart request.", e);
-            stack.next(method, instance);
-            return;
-        }
+		List<FileItem> fileItems;
+		try {
+			fileItems = fileUploadHandler.parseRequest(request);
+		} catch (FileUploadException e) {
+			logger
+					.warn(
+							"There was some problem parsing this multipart request, "
+									+ "or someone is not sending a RFC1867 compatible multipart request.",
+							e);
+			stack.next(method, instance);
+			return;
+		}
 
-        if (logger.isDebugEnabled()) {
-            logger.debug("Found [" + fileItems.size() + "] attributes in the multipart form submission. Parsing them.");
-        }
+		if (logger.isDebugEnabled()) {
+			logger
+					.debug("Found ["
+							+ fileItems.size()
+							+ "] attributes in the multipart form submission. Parsing them.");
+		}
 
-        new MultipartItemsProcessor(fileItems, request, parameters).process();
+		new MultipartItemsProcessor(fileItems, request, parameters).process();
 
-        stack.next(method, instance);
+		stack.next(method, instance);
 
-        // TODO should we delete the temporary files afterwards or onExit as
-        // done by
-        // now?maybe also a config in .properties
+		// TODO should we delete the temporary files afterwards or onExit as
+		// done by
+		// now?maybe also a config in .properties
 
-    }
+	}
 
-    private DiskFileItemFactory createFactoryForDiskBasedFileItems() {
-        DiskFileItemFactory factory = new DiskFileItemFactory(4096 * 16, this.temporaryDirectory);
-        logger.debug("Using repository [" + factory.getRepository() + "]");
-        return factory;
-    }
+	private static DiskFileItemFactory createFactoryForDiskBasedFileItems(
+			File temporaryDirectory) {
+		DiskFileItemFactory factory = new DiskFileItemFactory(4096 * 16,
+				temporaryDirectory);
+		logger.debug("Using repository [" + factory.getRepository()
+				+ "] for file upload");
+		return factory;
+	}
 
-    public boolean accepts(ResourceMethod method) {
-        return FileUploadBase.isMultipartContent(new ServletRequestContext(request));
-    }
+	public boolean accepts(ResourceMethod method) {
+		return FileUploadBase.isMultipartContent(new ServletRequestContext(
+				request));
+	}
 
 }

b4fba222261b167b2e717a122d05b79a1e625edb
Paulo Silveira <paulo.silveira@caelum.com.br>
2009-06-29 14:21:20 -0300

    converter acceptor tested

--- vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/pico/ConverterAcceptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/pico/ConverterAcceptor.java
@@ -1,6 +1,8 @@
 package br.com.caelum.vraptor.ioc.pico;
 
 import br.com.caelum.vraptor.Convert;
+import br.com.caelum.vraptor.Converter;
+import br.com.caelum.vraptor.VRaptorException;
 import br.com.caelum.vraptor.core.Converters;
 
 public class ConverterAcceptor implements Acceptor {
@@ -11,9 +13,13 @@ public class ConverterAcceptor implements Acceptor {
 		this.converters = converters;
 	}
 
+	@SuppressWarnings("unchecked")
 	public void analyze(Class<?> type) {
 		if (type.isAnnotationPresent(Convert.class)) {
-
+			if (!Converter.class.isAssignableFrom(type)) {
+				throw new VRaptorException("converter does not implement Converter");
+			}
+			converters.register(Class.class.cast(type));
 		}
 	}
 }

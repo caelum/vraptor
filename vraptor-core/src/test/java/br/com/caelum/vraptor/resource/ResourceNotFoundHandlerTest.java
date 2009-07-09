de85c078a3020c912da2f99892b3e4d0f0375850
Sergio Lopes <slopes@gmail.com>
2009-07-09 04:13:23 -0300

    merging with kung's commit 3164278bd1e

--- vraptor-core/src/test/java/br/com/caelum/vraptor/resource/ResourceNotFoundHandlerTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/resource/ResourceNotFoundHandlerTest.java
@@ -5,8 +5,6 @@ import java.io.StringWriter;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Assert;
@@ -14,19 +12,22 @@ import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.vraptor.core.RequestInfo;
+import br.com.caelum.vraptor.http.MutableRequest;
 
 public class ResourceNotFoundHandlerTest {
 
 	private ResourceNotFoundHandler notFoundHandler;
 	private Mockery mockery;
-	private HttpServletResponse webResponse;
-	private RequestInfo request;
+    private MutableRequest webRequest;
+    private HttpServletResponse webResponse;
+    private RequestInfo request;
 
-	@Before
+    @Before
 	public void setUp() {
 		this.mockery = new Mockery();
-		this.webResponse = mockery.mock(HttpServletResponse.class);
-		this.request = new RequestInfo(null, null, webResponse);
+        this.webRequest = mockery.mock(MutableRequest.class);
+        this.webResponse = mockery.mock(HttpServletResponse.class);
+        this.request = new RequestInfo(null, webRequest, webResponse);
 		this.notFoundHandler = new DefaultResourceNotFoundHandler();
 	}
 	
@@ -35,13 +36,15 @@ public class ResourceNotFoundHandlerTest {
         final StringWriter writer = new StringWriter();
         mockery.checking(new Expectations() {
             {
+                one(webRequest).getRequestURI();
+                will(returnValue("/some/requested/component"));
                 one(webResponse).sendError(404);
                 one(webResponse).getWriter();
                 will(returnValue(new PrintWriter(writer)));
             }
         });
 		notFoundHandler.couldntFind(request);
-		Assert.assertTrue(writer.getBuffer().toString().contains("resource not found"));
+		Assert.assertTrue(writer.getBuffer().toString().contains("Nothing at URI: /some/requested/component"));
         mockery.assertIsSatisfied();
 	}
 }

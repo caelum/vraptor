9328e91e7eb94dc938422afd548ec68b6a65c1d3
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-07-13 16:02:36 -0300

    changing Path annotation

--- vraptor-core/src/main/java/br/com/caelum/vraptor/Path.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/Path.java
@@ -38,7 +38,7 @@ import java.lang.annotation.Target;
 /**
  * Sets a custom path to allow web requisitions to access this resource.<br>
  * To be used together with web methods annotations as Get, Post and so on.
- * 
+ *
  * @author Guilherme Silveira
  */
 @Target(ElementType.METHOD)
@@ -47,5 +47,13 @@ import java.lang.annotation.Target;
 public @interface Path {
 
     String value();
+
+    /**
+     * Used to decide which path will be tested first.
+     * Paths with priority 0 will be tested before paths with priority 1, and
+     * so on.
+     * @return
+     */
+    int priority() default Integer.MAX_VALUE;
 
 }

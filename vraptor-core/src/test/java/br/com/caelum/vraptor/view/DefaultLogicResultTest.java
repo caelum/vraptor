379445727814807b065a304b6b7c025575c873c7
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-07-22 15:40:48 -0300

    testing

--- vraptor-core/src/test/java/br/com/caelum/vraptor/view/DefaultLogicResultTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/view/DefaultLogicResultTest.java
@@ -33,6 +33,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
@@ -45,6 +46,7 @@ import br.com.caelum.vraptor.http.MutableRequest;
 import br.com.caelum.vraptor.http.TypeCreator;
 import br.com.caelum.vraptor.http.route.Router;
 import br.com.caelum.vraptor.proxy.DefaultProxifier;
+import br.com.caelum.vraptor.resource.ResourceMethod;
 
 public class DefaultLogicResultTest {
 
@@ -78,11 +80,52 @@ public class DefaultLogicResultTest {
         this.request = mockery.mock(MutableRequest.class);
         this.context = mockery.mock(ServletContext.class);
         this.creator = mockery.mock(TypeCreator.class);
+
         this.logicResult = new DefaultLogicResult(new DefaultProxifier(), router, context, request, response, creator);
     }
 
+	private void ignoreFlashScope() {
+		mockery.checking(new Expectations() {
+			{
+				HttpSession session = mockery.mock(HttpSession.class);
+				ignoring(session);
+
+				allowing(creator).instanceWithParameters(with(any(ResourceMethod.class)), with(any(Object[].class)));
+				allowing(request).getSession(); will(returnValue(session));
+			}
+		});
+	}
+
+	@Test
+	public void shouldPutParametersOnFlashScopeOnForward() throws Exception {
+
+		shouldPutOnFlashScope();
+		logicResult.forwardTo(MyComponent.class).base();
+		mockery.assertIsSatisfied();
+	}
+	@Test
+	public void shouldPutParametersOnFlashScopeOnRedirect() throws Exception {
+
+		shouldPutOnFlashScope();
+		logicResult.redirectTo(MyComponent.class).base();
+		mockery.assertIsSatisfied();
+	}
+
+	private void shouldPutOnFlashScope() {
+		mockery.checking(new Expectations() {
+			{
+				HttpSession session = mockery.mock(HttpSession.class);
+				one(session).setAttribute(with(equal(DefaultLogicResult.FLASH_PARAMETERS)), with(any(Object.class)));
+
+				one(request).getSession(); will(returnValue(session));
+
+				ignoring(anything());
+			}
+		});
+	}
     @Test
     public void instantiatesUsingTheContainerAndAddsTheExecutionInterceptors() throws NoSuchMethodException, IOException, ServletException {
+    	ignoreFlashScope();
         final String url = "custom_url";
         mockery.checking(new Expectations() {
             {
@@ -100,6 +143,8 @@ public class DefaultLogicResultTest {
 
     @Test
     public void clientRedirectingWillRedirectToTranslatedUrl() throws NoSuchMethodException, IOException {
+    	ignoreFlashScope();
+
         final String url = "custom_url";
         mockery.checking(new Expectations() {
             {

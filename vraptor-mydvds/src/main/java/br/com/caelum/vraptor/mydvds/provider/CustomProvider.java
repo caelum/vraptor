d298d7598faf87e44c734acaeec644ae51f7914a
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-09-17 19:06:36 -0300

    using custom provider

--- vraptor-mydvds/src/main/java/br/com/caelum/vraptor/mydvds/provider/CustomProvider.java
+++ vraptor-mydvds/src/main/java/br/com/caelum/vraptor/mydvds/provider/CustomProvider.java
@@ -1,17 +1,16 @@
 package br.com.caelum.vraptor.mydvds.provider;
 
 import br.com.caelum.vraptor.ComponentRegistry;
+import br.com.caelum.vraptor.ioc.spring.SpringProvider;
 import br.com.caelum.vraptor.util.hibernate.SessionCreator;
 import br.com.caelum.vraptor.util.hibernate.SessionFactoryCreator;
-import br.com.caelum.vraptor.vraptor2.Provider;
 
-public class CustomProvider extends Provider {
+public class CustomProvider extends SpringProvider {
 
-    @Override
-    public void registerBundledComponents(ComponentRegistry registry) {
-        super.registerBundledComponents(registry);
-        registry.register(SessionCreator.class, SessionCreator.class);
-        registry.register(SessionFactoryCreator.class, SessionFactoryCreator.class);
-    }
+	@Override
+	protected void registerCustomComponents(ComponentRegistry registry) {
+		registry.register(SessionCreator.class, SessionCreator.class);
+		registry.register(SessionFactoryCreator.class, SessionFactoryCreator.class);
+	}
 
 }

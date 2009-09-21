d5a9c5e29b4dbbc726445c669b03a557fcdbb1fd
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-09-21 16:25:54 -0300

    more docs in english

--- vraptor-mydvds/src/main/java/br/com/caelum/vraptor/mydvds/provider/CustomProvider.java
+++ vraptor-mydvds/src/main/java/br/com/caelum/vraptor/mydvds/provider/CustomProvider.java
@@ -5,6 +5,14 @@ import br.com.caelum.vraptor.ioc.spring.SpringProvider;
 import br.com.caelum.vraptor.util.hibernate.SessionCreator;
 import br.com.caelum.vraptor.util.hibernate.SessionFactoryCreator;
 
+/**
+ * You can extend SpringProvider (or PicoProvider) in order to register
+ * optional components, like the Hibernate Session and SessionFactory
+ * Component Factories.
+ *
+ * @author Lucas Cavalcanti
+ *
+ */
 public class CustomProvider extends SpringProvider {
 
 	@Override

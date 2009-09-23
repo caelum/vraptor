73633ee22893d9a9695e9f06b93fedfcfcd644b2
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-09-23 16:43:44 -0300

    renaming folder

--- vraptor-mydvds/src/test/integration/br/com/caelum/vraptor/mydvds/IntegrationTestCase.java
+++ vraptor-mydvds/src/test/integration/br/com/caelum/vraptor/mydvds/IntegrationTestCase.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 
 import br.com.caelum.seleniumdsl.Browser;
 import br.com.caelum.seleniumdsl.htmlunit.HtmlUnitBrowser;
+import br.com.caelum.vraptor.mydvds.pages.RootPage;
 
 public abstract class IntegrationTestCase {
 
@@ -14,8 +15,9 @@ public abstract class IntegrationTestCase {
 		browser = new HtmlUnitBrowser("http://localhost:8080/vraptor-mydvds");
 	}
 
-	public void openRootPage() {
+	public RootPage openRootPage() {
 		browser.open("/");
+		return new RootPage(browser);
 	}
 
 }

dc820e06f40aa74d8b7cd87818bf5c0d28d4d69a
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-09-30 13:53:52 -0300

    licenses on java sources

--- vraptor-core/src/main/java/org/hamcrest/ResourceBundleDescription.java
+++ vraptor-core/src/main/java/org/hamcrest/ResourceBundleDescription.java
@@ -1,3 +1,19 @@
+/***
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hamcrest;
 
 import java.io.IOException;
@@ -6,9 +22,9 @@ import java.util.ResourceBundle;
 
 /**
  * A description which uses a resource bundle to i18n messages.
- * 
+ *
  * @author guilherme silveira
- * 
+ *
  */
 public class ResourceBundleDescription extends BaseDescription {
 
@@ -73,7 +89,7 @@ public class ResourceBundleDescription extends BaseDescription {
 	}
 
 	/**
-     * Appends the string straight to the buffer. 
+     * Appends the string straight to the buffer.
      */
 	protected void directAppend(String str) {
 		try {
@@ -82,7 +98,7 @@ public class ResourceBundleDescription extends BaseDescription {
 			throw new RuntimeException("Could not write description", e);
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		return out.toString();

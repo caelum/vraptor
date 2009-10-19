20906b71a11c5505c1096f9bf5cd72c9a17170b9
Paulo Silveira <paulo.silveira@caelum.com.br>
2009-10-19 19:45:05 -0200

    better messages when http method requested is not found,, closing issue 108

--- vraptor-core/src/main/java/br/com/caelum/vraptor/resource/HttpMethod.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/resource/HttpMethod.java
@@ -2,17 +2,17 @@
  * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
  * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- * 	http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package br.com.caelum.vraptor.resource;
@@ -45,9 +45,14 @@ public enum HttpMethod {
 
 	public static HttpMethod of(HttpServletRequest request) {
 		String methodName = request.getParameter(METHOD_PARAMETER);
-        if (methodName == null) {
-            methodName = request.getMethod();
-        }
-        return valueOf(methodName.toUpperCase());
+		if (methodName == null) {
+			methodName = request.getMethod();
+		}
+		try {
+			return valueOf(methodName.toUpperCase());
+		} catch (IllegalArgumentException e) {
+			// funny, but we need a better explanation
+			throw new IllegalArgumentException("HTTP Method not know: " + methodName, e);
+		}
 	}
 }

16bd40686e43085e657e535e97682df51555b624
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-11-05 17:34:27 -0200

    serialization

--- vraptor-core/src/main/java/br/com/caelum/vraptor/view/DefaultXmlSerialization.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/view/DefaultXmlSerialization.java
@@ -9,13 +9,14 @@ import br.com.caelum.vraptor.ioc.Component;
 @Component
 public class DefaultXmlSerialization implements XmlSerialization {
 
-	private final HttpServletResponse response;
+	final HttpServletResponse response;
 	
 	public DefaultXmlSerialization(HttpServletResponse response) {
 		this.response = response;
 	}
 	
 	public <T> XmlSerializer from(T object) throws IOException {
+		response.setContentType("application/xml");
 		XmlSerializer serializer = new XmlSerializer(response.getWriter());
 		return serializer;
 	}
--- vraptor-core/src/main/java/br/com/caelum/vraptor/view/XmlSerialization.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/view/XmlSerialization.java
@@ -1,11 +1,21 @@
 package br.com.caelum.vraptor.view;
 
+import java.io.IOException;
+
+import br.com.caelum.vraptor.View;
+
 /**
  * Basic xml serialization support using XmlSerializer.
  * 
  * @author guilherme silveira
  * @version 3.0.3
  */
-public interface XmlSerialization {
+public interface XmlSerialization extends View {
+
+	/**
+	 * Serializes this object to the clients writer.
+	 * @throws IOException
+	 */
+	public <T> XmlSerializer from(T object) throws IOException;
 
 }

c33e13dd3d5572010cbf5ce6fa4c001b76853cd6
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-11-09 14:14:05 -0200

    defaultxmlserialization passes restfulie

--- vraptor-core/src/main/java/br/com/caelum/vraptor/view/DefaultXmlSerialization.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/view/DefaultXmlSerialization.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.ioc.RequestScoped;
+import br.com.caelum.vraptor.rest.Restfulie;
 
 /**
  * The default implementation of xml serialization.<br/>
@@ -14,17 +16,20 @@ import br.com.caelum.vraptor.ioc.Component;
  * @since 3.0.3
  */
 @Component
+@RequestScoped
 public class DefaultXmlSerialization implements XmlSerialization {
 
-	final HttpServletResponse response;
+	private final HttpServletResponse response;
+	private final Restfulie restfulie;
 
-	public DefaultXmlSerialization(HttpServletResponse response) {
+	public DefaultXmlSerialization(HttpServletResponse response, Restfulie restfulie) {
 		this.response = response;
+		this.restfulie = restfulie;
 	}
 
 	public <T> XmlSerializer from(T object) throws IOException {
 		response.setContentType("application/xml");
-		XmlSerializer serializer = new XmlSerializer(response.getWriter()).from(object);
+		XmlSerializer serializer = new XmlSerializer(null, response.getWriter(), restfulie).from(object);
 		return serializer;
 	}
 

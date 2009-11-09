99862ee327000c357c0ec6dea6af6094bcd5c2fd
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-11-09 14:09:42 -0200

    added getter at restfulie interface

--- vraptor-core/src/main/java/br/com/caelum/vraptor/rest/Restfulie.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/rest/Restfulie.java
@@ -1,5 +1,16 @@
 package br.com.caelum.vraptor.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Restfulie {
+
+	public TransitionBuilder transition(String name);
+
+	public StateBuilder state(String name);
+
+	public List<Transition> getTransitions();
+
+	public List<State> getStates();
 
 }

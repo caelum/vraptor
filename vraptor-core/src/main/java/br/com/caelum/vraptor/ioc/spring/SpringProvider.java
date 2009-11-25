9277e6027c76c59aaa9d737033a8fe932ff7b96c
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-11-25 19:20:10 -0200

    fixing spring forward problem

--- vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/spring/SpringProvider.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/spring/SpringProvider.java
@@ -2,17 +2,17 @@
  * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
  * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- * 	http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package br.com.caelum.vraptor.ioc.spring;
 
@@ -42,21 +42,20 @@ public class SpringProvider implements ContainerProvider {
      * org.springframework.web.context.request.RequestContextListener has not been called.
      */
     public <T> T provideForRequest(RequestInfo request, Execution<T> execution) {
+    	if (springListenerAlreadyCalled()) {
+    		return execution.insideRequest(container);
+    	}
         VRaptorRequestHolder.setRequestForCurrentThread(request);
         T result;
         try {
-	        if (springListenerAlreadyCalled()) {
-	            result = execution.insideRequest(container);
-	        } else {
-	            ServletContext context = request.getServletContext();
-	            HttpServletRequest webRequest = request.getRequest();
-	            requestListener.requestInitialized(new ServletRequestEvent(context, webRequest));
-	            try {
-	            	result = execution.insideRequest(container);
-	            } finally {
-	            	requestListener.requestDestroyed(new ServletRequestEvent(context, webRequest));
-	            }
-	        }
+            ServletContext context = request.getServletContext();
+            HttpServletRequest webRequest = request.getRequest();
+            requestListener.requestInitialized(new ServletRequestEvent(context, webRequest));
+            try {
+            	result = execution.insideRequest(container);
+            } finally {
+            	requestListener.requestDestroyed(new ServletRequestEvent(context, webRequest));
+            }
         } finally {
         	VRaptorRequestHolder.resetRequestForCurrentThread();
         }

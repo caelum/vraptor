0d5e85c13c0da93593241f8db7a4c5d349f66666
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-11-26 16:18:43 -0200

    removing some TODOs

--- vraptor-core/src/test/java/br/com/caelum/vraptor/core/DefaultConvertersTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/core/DefaultConvertersTest.java
@@ -2,17 +2,17 @@
  * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
  * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- * 	http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package br.com.caelum.vraptor.core;
 
@@ -20,7 +20,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.typeCompatibleWith;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -137,7 +137,6 @@ public class DefaultConvertersTest {
     class WrongConverter implements Converter<String> {
 
         public String convert(String value, Class<? extends String> type, ResourceBundle bundle) {
-            // TODO Auto-generated method stub
             return null;
         }
     }
@@ -187,18 +186,18 @@ public class DefaultConvertersTest {
         assertThat(found.getClass(), is(typeCompatibleWith(MySecondConverter.class)));
         mockery.assertIsSatisfied();
     }
-    
+
     @Test
 	public void existsForWillReturnTrueForRegisteredConverters() throws Exception {
 		converters.register(MyConverter.class);
-		
+
 		mockery.checking(new Expectations() {
             {
                 allowing(container).instanceFor(MyConverter.class);
                 will(returnValue(new MyConverter()));
             }
         });
-		
+
 		assertTrue(converters.existsFor(MyData.class, container));
 	}
 
--- vraptor-core/src/test/java/br/com/caelum/vraptor/test/HttpServletRequestMock.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/test/HttpServletRequestMock.java
@@ -2,17 +2,17 @@
  * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
  * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- * 	http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package br.com.caelum.vraptor.test;
 
@@ -101,27 +101,22 @@ public class HttpServletRequestMock implements MutableRequest {
     }
 
     public long getDateHeader(String s) {
-        // TODO
         return 0;
     }
 
     public String getHeader(String s) {
-        // TODO
         return null;
     }
 
     public Enumeration getHeaders(String s) {
-        // TODO
         return null;
     }
 
     public Enumeration getHeaderNames() {
-        // TODO
         return null;
     }
 
     public int getIntHeader(String s) {
-        // TODO
         return 0;
     }
 
@@ -174,7 +169,6 @@ public class HttpServletRequestMock implements MutableRequest {
     }
 
     public boolean isUserInRole(String s) {
-        // TODO
         return false;
     }
 

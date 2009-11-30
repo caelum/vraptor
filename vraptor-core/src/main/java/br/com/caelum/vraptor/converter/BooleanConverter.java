d041f57c0bdc5abfd66d68923541d4349516af3b
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-11-30 16:19:48 -0200

    fixing boolean converter

--- vraptor-core/src/main/java/br/com/caelum/vraptor/converter/BooleanConverter.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/converter/BooleanConverter.java
@@ -2,17 +2,17 @@
  * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
  * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- * 	http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package br.com.caelum.vraptor.converter;
@@ -42,7 +42,7 @@ public class BooleanConverter implements Converter<Boolean> {
 	private static final Set<String> IS_FALSE = new HashSet<String>(Arrays.asList("FALSE", "0", "NO", "N", "OFF"));
 
 	public Boolean convert(String value, Class<? extends Boolean> type, ResourceBundle bundle) {
-		if (value == null) {
+		if (value == null || "".equals(value)) {
 			return null;
 		}
 		value = value.toUpperCase();

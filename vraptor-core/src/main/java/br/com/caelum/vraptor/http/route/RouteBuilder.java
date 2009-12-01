89eb474fa958b4d062dcdba60e76aa082a38725a
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-12-01 19:52:50 -0200

    regexes in paths

--- vraptor-core/src/main/java/br/com/caelum/vraptor/http/route/RouteBuilder.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/http/route/RouteBuilder.java
@@ -2,17 +2,17 @@
  * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
  * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- * 	http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 /***
  *
@@ -171,6 +171,12 @@ public class RouteBuilder {
 		for (Entry<String, Class<?>> entry : types.entrySet()) {
 			if (!builder.parameters.containsKey(entry.getKey())) {
 				builder.withParameter(entry.getKey()).ofType(entry.getValue());
+			}
+		}
+		for (String parameter : parameters) {
+			String[] split = parameter.split(":");
+			if (split.length >= 2 && !builder.parameters.containsKey(parameter)) {
+				builder.withParameter(parameter).matching(split[1]);
 			}
 		}
 	}

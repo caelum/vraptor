24112b3083665738169be855e896204a5f4ffa27
Paulo Silveira <paulo.silveira@caelum.com.br>
2009-12-30 19:02:42 -0200

    better blank project for GAE

--- vraptor-blank-project-gae/src/br/com/caelum/vraptor/blank/infra/AppEngineMultipartConfig.java
+++ vraptor-blank-project-gae/src/br/com/caelum/vraptor/blank/infra/AppEngineMultipartConfig.java
@@ -10,12 +10,10 @@ import br.com.caelum.vraptor.ioc.Component;
 @ApplicationScoped
 public class AppEngineMultipartConfig implements MultipartConfig {
 
-	@Override
 	public File getDirectory() {
 		return null;
 	}
 
-	@Override
 	public long getSizeLimit() {
 		return Integer.MAX_VALUE;
 	}

b72bf71720aa18112fa7542a05ecda5b1233df7c
Paulo Silveira <paulo.silveira@caelum.com.br>
2010-01-14 18:41:04 -0200

    test for applicationconfiguration

---
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/config/ApplicationConfigurationTest.java
@@ -0,0 +1,46 @@
+package br.com.caelum.vraptor.config;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class ApplicationConfigurationTest {
+
+	@Mock
+	HttpServletRequest request;
+	private ApplicationConfiguration configuration;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		this.configuration = new ApplicationConfiguration(request);
+	}
+
+	@Test
+	public void shouldNotUsePortWhenPortIs80() {
+
+		when(request.getServerName()).thenReturn("caelum.com.br");
+		when(request.getServerPort()).thenReturn(80);
+		when(request.getContextPath()).thenReturn("/context/");
+
+		assertEquals("http://caelum.com.br/context/", configuration.getApplicationPath());
+
+	}
+
+	@Test
+	public void shouldGiveFullUrlWithPortWhenPortIsNot80() {
+		when(request.getServerName()).thenReturn("caelum.com.br");
+		when(request.getServerPort()).thenReturn(8080);
+		when(request.getContextPath()).thenReturn("/context/");
+
+		assertEquals("http://caelum.com.br:8080/context/", configuration.getApplicationPath());
+
+	}
+
+}

9ad823735fda331de2ae0f81daa4e05973dca368
Paulo Silveira <paulo.silveira@caelum.com.br>
2010-01-14 19:15:16 -0200

    test for http/https

--- vraptor-core/src/test/java/br/com/caelum/vraptor/config/ApplicationConfigurationTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/config/ApplicationConfigurationTest.java
@@ -24,23 +24,22 @@ public class ApplicationConfigurationTest {
 
 	@Test
 	public void shouldNotUsePortWhenPortIs80() {
-
+		when(request.getScheme()).thenReturn("http");
 		when(request.getServerName()).thenReturn("caelum.com.br");
 		when(request.getServerPort()).thenReturn(80);
 		when(request.getContextPath()).thenReturn("/context/");
 
 		assertEquals("http://caelum.com.br/context/", configuration.getApplicationPath());
-
 	}
 
 	@Test
 	public void shouldGiveFullUrlWithPortWhenPortIsNot80() {
+		when(request.getScheme()).thenReturn("http");
 		when(request.getServerName()).thenReturn("caelum.com.br");
 		when(request.getServerPort()).thenReturn(8080);
 		when(request.getContextPath()).thenReturn("/context/");
 
 		assertEquals("http://caelum.com.br:8080/context/", configuration.getApplicationPath());
-
 	}
 
 }

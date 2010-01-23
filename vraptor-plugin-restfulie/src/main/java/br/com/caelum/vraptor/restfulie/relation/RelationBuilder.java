433f61f69f800d7f984d99241e29dfe29077c47b
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-01-23 19:01:54 -0200

    small doc on at

--- vraptor-plugin-restfulie/src/main/java/br/com/caelum/vraptor/restfulie/relation/RelationBuilder.java
+++ vraptor-plugin-restfulie/src/main/java/br/com/caelum/vraptor/restfulie/relation/RelationBuilder.java
@@ -1,5 +1,10 @@
 package br.com.caelum.vraptor.restfulie.relation;
 
+/**
+ * Responsible for allowing clients to create relations
+ * @author guilherme silveira
+ * @author pedro matiello
+ */
 public interface RelationBuilder {
 
 	<T> T uses(Class<T> type);

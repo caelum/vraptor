0ac13a6cca99caf3e89f26f721ff74a836887930
Paulo Silveira <paulo.silveira@caelum.com.br>
2010-01-28 17:09:00 -0200

    better logging

--- vraptor-core/src/main/java/br/com/caelum/vraptor/vraptor2/ComponentRoutesParser.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/vraptor2/ComponentRoutesParser.java
@@ -2,17 +2,17 @@
  * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
  * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- * 	http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package br.com.caelum.vraptor.vraptor2;
@@ -70,7 +70,7 @@ public class ComponentRoutesParser implements RoutesParser {
         if(!Info.isOldComponent(resource)) {
         	return delegate.rulesFor(resource);
         }
-        logger.warn("Old component found, remember to migrate to vraptor3: " + type.getName());
+        logger.warn("VRaptor2 component found, you should migrate it to VRaptor3: " + type.getName());
 		registerRulesFor(type, type, routes);
         parse(type, type);
 		return routes;

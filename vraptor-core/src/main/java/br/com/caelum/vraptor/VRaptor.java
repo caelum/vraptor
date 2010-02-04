e978f2b1bc6c2d98100f7e76f7e46e3fed36ab18
Paulo Silveira <paulo.silveira@caelum.com.br>
2010-02-04 19:53:28 -0200

    more logs

--- vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
@@ -68,7 +68,6 @@ public class VRaptor implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
 			ServletException {
 
-
 		if (!(req instanceof HttpServletRequest) || !(res instanceof HttpServletResponse)) {
 			throw new ServletException(
 					"VRaptor must be run inside a Servlet environment. Portlets and others aren't supported.");

846d31cc06cbd49f043f462b4d12bbb8494e271f
Paulo Silveira <paulo.silveira@caelum.com.br>
2010-02-04 12:06:48 -0200

    better toStrings

--- vraptor-core/src/main/java/br/com/caelum/vraptor/http/route/FixedMethodStrategy.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/http/route/FixedMethodStrategy.java
@@ -82,7 +82,7 @@ public class FixedMethodStrategy implements Route {
 
 	@Override
 	public String toString() {
-		return "[FixedMethodStrategy: " + originalUri + " " + Stringnifier.simpleNameFor(resourceMethod.getMethod())
-				+ " " + methods + "]";
+		return String.format("[FixedMethodStrategy: %-70s   %-70s    %s]", originalUri, Stringnifier
+				.simpleNameFor(resourceMethod.getMethod()), methods);
 	}
 }

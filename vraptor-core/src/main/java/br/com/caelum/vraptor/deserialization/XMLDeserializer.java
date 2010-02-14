50e5c071eb914ec8a80d5746634ab77ffc07c677
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-02-14 19:25:04 -0200

    allows xstream ready to use retrieval

--- vraptor-core/src/main/java/br/com/caelum/vraptor/deserialization/XMLDeserializer.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/deserialization/XMLDeserializer.java
@@ -15,6 +15,10 @@
  */
 package br.com.caelum.vraptor.deserialization;
 
+import java.lang.reflect.Method;
+
+import com.thoughtworks.xstream.XStream;
+
 /**
  * Deserializes xml
  * @author Lucas Cavalcanti, Cecilia Fernandes

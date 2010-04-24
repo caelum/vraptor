fa6c19d40da8139b0f9fcbb26ec5292f593e7336
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2010-04-24 13:34:46 -0300

    java 5 compliance

--- vraptor-core/src/main/java/br/com/caelum/vraptor/validator/HibernateValidator3.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/validator/HibernateValidator3.java
@@ -2,17 +2,17 @@
  * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
  * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- *  http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package br.com.caelum.vraptor.validator;
 
@@ -25,7 +25,7 @@ import org.hibernate.validator.InvalidValue;
 /**
  * Implements the {@link BeanValidator} using the Hibernate Validator 3.x. This implementation will be enable by vraptor
  * when the hibernate validator classes is locale in classpath.
- * 
+ *
  * @author Guilherme Silveira
  * @since 3.1.2
  */
@@ -35,7 +35,6 @@ public class HibernateValidator3
     private static final ValidatorLocator locator = new ValidatorLocator();
 
     @SuppressWarnings("unchecked")
-    @Override
     public List<Message> validate(Object object) {
         List<Message> errors = new ArrayList<Message>();
         ClassValidator<Object> validator = (ClassValidator<Object>) locator.getValidator(object.getClass(), null);

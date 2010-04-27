e13758a16a6c462a0506c5090db98e88426bb320
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2010-04-27 00:19:33 -0300

    changing version to 3.1.2

--- vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
@@ -101,7 +101,7 @@ public class VRaptor implements Filter {
 		servletContext = cfg.getServletContext();
 		BasicConfiguration config = new BasicConfiguration(servletContext);
 		init(config.getProvider(), new DefaultStaticContentHandler(servletContext));
-		logger.info("VRaptor 3.1.2-SNAP successfuly initialized");
+		logger.info("VRaptor 3.1.2 successfuly initialized");
 	}
 
 	void init(ContainerProvider provider, StaticContentHandler handler) {

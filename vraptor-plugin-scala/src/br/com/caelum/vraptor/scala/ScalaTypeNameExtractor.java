a7c605de6370cffad73058a8b7eb27267120061c
Pedro Matiello <pmatiello@gmail.com>
2010-05-07 15:23:08 -0300

    Scala's Collection[Type] now injected in view as typeList

---
+++ vraptor-plugin-scala/src/br/com/caelum/vraptor/scala/ScalaTypeNameExtractor.java
@@ -0,0 +1,79 @@
+/***
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package br.com.caelum.vraptor.scala;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
+import java.util.Collection;
+
+import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
+import br.com.caelum.vraptor.util.StringUtils;
+import br.com.caelum.vraptor.interceptor.DefaultTypeNameExtractor;
+
+/**
+ * Scala-compatible implementation for {@link TypeNameExtractor}.
+ * It decapitalizes the name of the type, or if the type is a generic collection,
+ * uses the decapitalized name of generic type plus 'List'.
+ *
+ * @author Pedro Matiello <pmatiello@gmail.com>
+ * @author Alberto Souza <alberto.souza@caelum.com.br>
+ */
+@ApplicationScoped
+@Component
+public class ScalaTypeNameExtractor extends DefaultTypeNameExtractor {
+
+	public String nameFor(Type generic) {
+		if (generic instanceof ParameterizedType) {
+			ParameterizedType type = (ParameterizedType) generic;
+			Class<?> raw = (Class<?>) type.getRawType();
+			if (Collection.class.isAssignableFrom(raw)) {
+				return nameFor(type.getActualTypeArguments()[0]) + "List";
+			} else if (scala.collection.Seq.class.isAssignableFrom(raw)) {
+				return nameFor(type.getActualTypeArguments()[0]) + "List";
+			}
+			return nameFor(raw);
+		}
+
+		if (generic instanceof WildcardType) {
+			WildcardType wild = (WildcardType) generic;
+			if ((wild.getLowerBounds().length != 0)) {
+				return nameFor(wild.getLowerBounds()[0]);
+			} else {
+				return nameFor(wild.getUpperBounds()[0]);
+			}
+		}
+
+		if (generic instanceof TypeVariable<?>) {
+			TypeVariable<?> variable = (TypeVariable<?>) generic;
+			return StringUtils.lowercaseFirst(variable.getName());
+		}
+
+		Class<?> raw = (Class<?>) generic;
+
+
+		if (raw.isArray()) {
+			return nameFor(raw.getComponentType()) + "List";
+		}
+
+		String name = raw.getSimpleName();
+
+		return StringUtils.lowercaseFirst(name);
+	}
+
+}

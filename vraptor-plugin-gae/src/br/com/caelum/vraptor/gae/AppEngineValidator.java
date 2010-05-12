142aa1f0c44642a144009363d7a3cad418afdf94
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2010-05-12 10:23:53 -0300

    licences and AppEngineValidator extending DefaultValidator

--- vraptor-plugin-gae/src/br/com/caelum/vraptor/gae/AppEngineValidator.java
+++ vraptor-plugin-gae/src/br/com/caelum/vraptor/gae/AppEngineValidator.java
@@ -1,65 +1,46 @@
+/***
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package br.com.caelum.vraptor.gae;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import br.com.caelum.vraptor.Result;
-import br.com.caelum.vraptor.Validator;
 import br.com.caelum.vraptor.View;
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.ioc.RequestScoped;
+import br.com.caelum.vraptor.proxy.Proxifier;
 import br.com.caelum.vraptor.validator.BeanValidator;
-import br.com.caelum.vraptor.validator.Message;
-import br.com.caelum.vraptor.validator.Validations;
+import br.com.caelum.vraptor.validator.DefaultValidator;
+import br.com.caelum.vraptor.validator.Outjector;
 import br.com.caelum.vraptor.view.ValidationViewsFactory;
 
 @Component
 @RequestScoped
-public class AppEngineValidator implements Validator {
+public class AppEngineValidator extends DefaultValidator {
 
-    private final Result result;
-	private final List<Message> errors = new ArrayList<Message>();
-	private final ValidationViewsFactory viewsFactory;
-	private final List<BeanValidator> beanValidators;
-
-    public AppEngineValidator(Result result, ValidationViewsFactory factory, List<BeanValidator> beanValidators) {
-        this.result = result;
-		this.viewsFactory = factory;
-		this.beanValidators = beanValidators;
+    public AppEngineValidator(Result result, ValidationViewsFactory factory, List<BeanValidator> beanValidators, Outjector outjector, Proxifier proxifier) {
+    	super(result, factory, outjector, proxifier, beanValidators);
     }
 
-    // TODO: do not use String consequences anymore
-    // TODO: on error action should be defined by the onError method
-    public void checking(Validations validations) {
-        addAll(validations.getErrors());
-    }
-
-
+    @Override
     public <T extends View> T onErrorUse(Class<T> view) {
     	if (!hasErrors()) {
     		return view.cast(new AppEngineMockedPage()); //ignore anything, no errors occurred
     	}
-    	result.include("errors", errors);
-    	return viewsFactory.instanceFor(view, errors);
-    }
-
-    public void addAll(Collection<? extends Message> message) {
-		this.errors.addAll(message);
-	}
-
-    public void add(Message message) {
-    	this.errors.add(message);
-    }
-
-	public boolean hasErrors() {
-		return !errors.isEmpty();
-	}
-
-	public void validate(Object object) {
-        for (BeanValidator validator : beanValidators) {
-            addAll(validator.validate(object));
-        }
+    	return super.onErrorUse(view);
     }
 
 }

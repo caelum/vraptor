52d00ed297feb1a526e18089430b0eee9afe2b90
Rodolfo Liviero <rodolfoliviero@gmail.com>
2010-07-18 12:08:39 -0300

    refactor and tests to LocalDateConverter

--- vraptor-core/src/main/java/br/com/caelum/vraptor/converter/jodatime/LocalDateConverter.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/converter/jodatime/LocalDateConverter.java
@@ -17,7 +17,7 @@
 package br.com.caelum.vraptor.converter.jodatime;
 
 import java.text.MessageFormat;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import org.joda.time.LocalDate;
@@ -25,26 +25,26 @@ import org.joda.time.LocalDate;
 import br.com.caelum.vraptor.Convert;
 import br.com.caelum.vraptor.Converter;
 import br.com.caelum.vraptor.converter.ConversionError;
-import br.com.caelum.vraptor.converter.LocaleBasedCalendarConverter;
+import br.com.caelum.vraptor.core.RequestInfo;
 
 @Convert(LocalDate.class)
 public class LocalDateConverter implements Converter<LocalDate> {
 
-	private final LocaleBasedCalendarConverter delegate;
+	private RequestInfo request;
 
-	public LocalDateConverter(LocaleBasedCalendarConverter delegate) {
-		this.delegate = delegate;
+	public LocalDateConverter(RequestInfo request) {
+		this.request = request;
 	}
 
 	public LocalDate convert(String value, Class<? extends LocalDate> type, ResourceBundle bundle) {
-		Calendar calendar = delegate.convert(value, Calendar.class, bundle);
-		if (calendar == null) {
-			return null;
-		}
 		try {
-            return LocalDate.fromCalendarFields(calendar);
-        } catch (Exception e) {
-        	throw new ConversionError(MessageFormat.format(bundle.getString("is_not_a_valid_date"), value));
-        }
+			Date date = new LocaleBasedJodaTimeConverter(request).convert(value, type);
+			if (date == null) {
+				return null;
+			}
+			return LocalDate.fromDateFields(date);
+		} catch (Exception e) {
+			throw new ConversionError(MessageFormat.format(bundle.getString("is_not_a_valid_date"), value));
+		}
 	}
 }

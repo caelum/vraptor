a63dca35be6417c0e36666329ce4d89ce6f7a7b0
Rodolfo Liviero <rodolfoliviero@gmail.com>
2010-07-18 22:39:11 +0800

    fix LocalTimeConverter

---
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/converter/jodatime/LocalTimeConverterTest.java
@@ -0,0 +1,89 @@
+package br.com.caelum.vraptor.converter.jodatime;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+import java.util.ResourceBundle;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletContext;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.joda.time.LocalTime;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.vraptor.converter.ConversionError;
+import br.com.caelum.vraptor.core.RequestInfo;
+import br.com.caelum.vraptor.http.MutableRequest;
+
+/**
+ * Tests to {@link LocalTimeConverter}.
+ */
+public class LocalTimeConverterTest {
+
+	private Mockery mockery;
+	private MutableRequest request;
+	private ServletContext context;
+	private ResourceBundle bundle;
+	private LocalTimeConverter converter;
+
+	@Before
+	public void setup() {
+		this.mockery = new Mockery();
+
+		this.request = mockery.mock(MutableRequest.class);
+		this.context = mockery.mock(ServletContext.class);
+		this.bundle = ResourceBundle.getBundle("messages");
+		FilterChain chain = mockery.mock(FilterChain.class);
+
+		final RequestInfo webRequest = new RequestInfo(context, chain, request, null);
+
+		this.converter = new LocalTimeConverter(webRequest);
+	}
+
+	@Test
+	public void shouldBeAbleToConvert() {
+		mockery.checking(new Expectations() {
+			{
+				exactly(2).of(request).getAttribute("javax.servlet.jsp.jstl.fmt.locale.request");
+				will(returnValue("pt_br"));
+			}
+		});
+
+		assertThat(converter.convert("15:38", LocalTime.class, bundle),
+				is(equalTo(new LocalTime(15, 38))));
+
+		mockery.assertIsSatisfied();
+	}
+
+	@Test
+	public void shouldBeAbleToConvertEmpty() {
+		assertThat(converter.convert("", LocalTime.class, bundle), is(nullValue()));
+		mockery.assertIsSatisfied();
+	}
+
+	@Test
+	public void shouldBeAbleToConvertNull() {
+		assertThat(converter.convert(null, LocalTime.class, bundle), is(nullValue()));
+		mockery.assertIsSatisfied();
+	}
+
+	@Test
+	public void shouldThrowExceptionWhenUnableToParse() {
+		mockery.checking(new Expectations() {
+			{
+				exactly(2).of(request).getAttribute("javax.servlet.jsp.jstl.fmt.locale.request");
+				will(returnValue("pt_br"));
+			}
+		});
+		try {
+			converter.convert("xx:yy:ff", LocalTime.class, bundle);
+		} catch (ConversionError e) {
+			assertThat(e.getMessage(), is(equalTo("xx:yy:ff is not a valid time.")));
+		}
+	}
+}

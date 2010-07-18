2bade870d7640c6e21bbfb4ee349dc80154c342d
Rodolfo Liviero <rodolfoliviero@gmail.com>
2010-07-18 11:39:11 -0300

    fix LocalTimeConverter

--- vraptor-core/src/main/java/br/com/caelum/vraptor/converter/jodatime/LocalTimeConverter.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/converter/jodatime/LocalTimeConverter.java
@@ -17,7 +17,7 @@
 package br.com.caelum.vraptor.converter.jodatime;
 
 import java.text.MessageFormat;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import org.joda.time.LocalTime;
@@ -25,26 +25,26 @@ import org.joda.time.LocalTime;
 import br.com.caelum.vraptor.Convert;
 import br.com.caelum.vraptor.Converter;
 import br.com.caelum.vraptor.converter.ConversionError;
-import br.com.caelum.vraptor.converter.LocaleBasedCalendarConverter;
+import br.com.caelum.vraptor.core.RequestInfo;
 
 @Convert(LocalTime.class)
 public class LocalTimeConverter implements Converter<LocalTime> {
+	
+    private RequestInfo request;
 
-	private final LocaleBasedCalendarConverter delegate;
-
-	public LocalTimeConverter(LocaleBasedCalendarConverter delegate) {
-		this.delegate = delegate;
+	public LocalTimeConverter(RequestInfo request) {
+		this.request = request;
 	}
 
 	public LocalTime convert(String value, Class<? extends LocalTime> type, ResourceBundle bundle) {
-		Calendar calendar = delegate.convert(value, Calendar.class, bundle);
-		if (calendar == null) {
-			return null;
-		}
-		try {
-            return LocalTime.fromCalendarFields(calendar);
+        try {
+        	Date time = new LocaleBasedJodaTimeConverter(request).convert(value, type);
+        	if (time == null) {
+        		return null;
+        	}
+        	return LocalTime.fromDateFields(time);
         } catch (Exception e) {
-        	throw new ConversionError(MessageFormat.format(bundle.getString("is_not_a_valid_date"), value));
-        }
+			throw new ConversionError(MessageFormat.format(bundle.getString("is_not_a_valid_time"), value));
+        } 
 	}
 }
---
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/converter/jodatime/LocaleBasedJodaTimeConverter.java
@@ -0,0 +1,40 @@
+package br.com.caelum.vraptor.converter.jodatime;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Locale;
+
+import org.joda.time.base.BaseLocal;
+
+import br.com.caelum.vraptor.converter.JstlWrapper;
+import br.com.caelum.vraptor.core.RequestInfo;
+
+class LocaleBasedJodaTimeConverter {
+
+	private JstlWrapper jstlWrapper = new JstlWrapper();
+	private RequestInfo request;
+
+	public LocaleBasedJodaTimeConverter(RequestInfo request) {
+		this.request = request;
+	}
+
+	public Date convert(String value, Class<? extends BaseLocal> type) throws ParseException {
+		if (value == null || value.equals("")) {
+			return null;
+		}
+		return getDateFormat(type).parse(value);
+	}
+	
+	public Locale getLocale() {
+		Locale locale = jstlWrapper.findLocale(request);
+		if (locale == null) {
+			locale = Locale.getDefault();
+		}
+		return locale;
+	}
+	
+	private DateFormat getDateFormat(Class<? extends BaseLocal> type) {
+		return DateFormat.getTimeInstance(DateFormat.SHORT, getLocale());
+	}
+}

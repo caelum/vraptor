a75885c3d6f7296fb664d8e3406f2b082b30539a
Rodolfo Liviero <rodolfoliviero@gmail.com>
2010-09-13 10:53:34 +0800

    fix scala build

--- vraptor-plugin-scala/src/br/com/caelum/vraptor/scala/ScalaRoutes.scala
+++ vraptor-plugin-scala/src/br/com/caelum/vraptor/scala/ScalaRoutes.scala
@@ -1,17 +1,14 @@
 package br.com.caelum.vraptor.scala
 
 import br.com.caelum.vraptor.ioc.ApplicationScoped;
-import br.com.caelum.vraptor.proxy.Proxifier;
 import br.com.caelum.vraptor.http.route.PathAnnotationRoutesParser;
-import br.com.caelum.vraptor.http.route.TypeFinder;
-import br.com.caelum.vraptor.ioc.Component;
 import java.lang.reflect.Method;
+import br.com.caelum.vraptor.http.route.Router
 
 @ApplicationScoped
-@Component
 /** This is a Scala-compatible path parser that uses Path annotations in order to create the routing rules.
  */
-class ScalaRoutes(proxifier:Proxifier, finder:TypeFinder) extends PathAnnotationRoutesParser(proxifier, finder) {
+class ScalaRoutes(router:Router) extends PathAnnotationRoutesParser(router) {
 
 	override protected def isEligible(method:Method) =	super.isEligible(method) &&
 															!method.getDeclaringClass().equals(classOf[AnyRef]) &&

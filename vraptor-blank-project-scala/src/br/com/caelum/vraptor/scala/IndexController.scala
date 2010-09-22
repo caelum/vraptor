ca23dca5a286514dc2994bcf9566d5a3c95d6648
Pedro Matiello <pmatiello@gmail.com>
2010-09-22 19:56:33 -0300

    Blank project working. A simple model was added. SSP and JSP views are used.

--- vraptor-blank-project-scala/src/br/com/caelum/vraptor/scala/IndexController.scala
+++ vraptor-blank-project-scala/src/br/com/caelum/vraptor/scala/IndexController.scala
@@ -1,14 +1,17 @@
-package br.com.caelum.scala
-
-import br.com.caelum.vraptor.Resource;
-import br.com.caelum.vraptor.Get;
-import br.com.caelum.vraptor.Path;
+package br.com.caelum.vraptor.scala
+import br.com.caelum.vraptor.{Path, Get, Post, Resource};
 
 @Resource
 class IndexController {
 
 	@Get @Path(Array("/"))
-	def index = "It works!"
+	def index = new MyModel("It works!")
+	
+	@Post @Path(Array("/"))
+	def index(myModel:MyModel) = myModel
+		
+	@Get @Path(Array("/jsp"))
+	def jsp = new MyModel("It works with JSP too!")
 	
 	@Get @Path(Array("/list"))
 	def list = List("It", "works", "with", "lists")
---
+++ vraptor-blank-project-scala/src/br/com/caelum/vraptor/scala/MyModel.scala
@@ -0,0 +1,5 @@
+package br.com.caelum.vraptor.scala
+
+import scala.reflect.BeanProperty
+
+case class MyModel(@BeanProperty data:String)

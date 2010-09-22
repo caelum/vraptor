2c7d68e8b31554dc886a4f027bd21553f7425464
Pedro Matiello <pmatiello@gmail.com>
2010-09-22 20:28:10 -0300

    Removed new from instantiations of MyModel (it's a case class).

--- vraptor-blank-project-scala/src/br/com/caelum/vraptor/scala/IndexController.scala
+++ vraptor-blank-project-scala/src/br/com/caelum/vraptor/scala/IndexController.scala
@@ -8,13 +8,13 @@ import br.com.caelum.vraptor.{Path, Get, Post, Resource};
 class IndexController {
 
 	@Get @Path(Array("/"))
-	def index = new MyModel("It works!")
+	def index = MyModel("It works!")
 	
 	@Post @Path(Array("/"))
 	def index(myModel:MyModel) = myModel
 		
 	@Get @Path(Array("/jsp"))
-	def jsp = new MyModel("It works with JSP too!")
+	def jsp = MyModel("It works with JSP too!")
 	
 	@Get @Path(Array("/list"))
 	def list = List("It", "works", "with", "lists")

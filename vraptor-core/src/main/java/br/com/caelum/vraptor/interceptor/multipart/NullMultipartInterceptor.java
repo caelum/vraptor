407a34c13ffc8c2380825bde1bbbfa73c54bc59e
Otávio Garcia <otavio@otavio.com.br>
2010-10-25 23:14:10 -0200

    warn when no upload handler is found

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/multipart/NullMultipartInterceptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/multipart/NullMultipartInterceptor.java
@@ -1,7 +1,11 @@
 package br.com.caelum.vraptor.interceptor.multipart;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.caelum.vraptor.InterceptionException;
-import br.com.caelum.vraptor.Lazy;
 import br.com.caelum.vraptor.core.InterceptorStack;
 import br.com.caelum.vraptor.resource.ResourceMethod;
 
@@ -12,15 +16,30 @@ import br.com.caelum.vraptor.resource.ResourceMethod;
  * @since 3.1.3
  * @see CommonsUploadMultipartInterceptor
  */
-@Lazy
 public class NullMultipartInterceptor implements MultipartInterceptor {
+    
+    private static final Logger logger = LoggerFactory.getLogger(NullMultipartInterceptor.class);
+    
+    private final HttpServletRequest request;
+    
+    public NullMultipartInterceptor(HttpServletRequest request) {
+        this.request = request;
+    }
 
+    /**
+     * Only accept requests that contains multipart headers.
+     */
     public boolean accepts(ResourceMethod method) {
-        return false;
+        String contentType = request.getContentType();
+        return contentType != null && contentType.startsWith(Servlet3MultipartInterceptor.ACCEPT_MULTIPART);
     }
 
     public void intercept(InterceptorStack stack, ResourceMethod method, Object resourceInstance)
         throws InterceptionException {
+        logger.warn("You are willing to upload a file, but there is no commons-fileupload" +
+            " or servlet3 handlers registered. Please add the commons-fileupload in your classpath" +
+            " or use a Servlet 3 Container");
         
+        stack.next(method, resourceInstance);
     }
 }

9eba567a617ee05a0a38adc475b2225325d73763
guilherme silveira & adriano almeida <guilherme.silveira@caelum.com.br>
2010-11-19 21:36:56 -0200

    docs

--- vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/ContainerProvider.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/ContainerProvider.java
@@ -22,12 +22,17 @@ import javax.servlet.ServletContext;
 import br.com.caelum.vraptor.core.Execution;
 import br.com.caelum.vraptor.core.RequestInfo;
 
+/**
+ * A container provider, i.e. spring, pico, guice, tapestry and so on.
+ * 
+ * @author guilherme silveira
+ */
 public interface ContainerProvider {
 
-    <T> T provideForRequest(RequestInfo vraptorRequest, Execution<T> execution);
+	<T> T provideForRequest(RequestInfo vraptorRequest, Execution<T> execution);
 
-    void stop();
+	void stop();
 
-    void start(ServletContext context);
+	void start(ServletContext context);
 
 }

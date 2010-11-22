18f082428deb98558f266738e2a8bbc17bcdb0f2
guilherme silveira & adriano almeida <guilherme.silveira@caelum.com.br>
2010-11-22 09:59:58 -0200

    removing my crap

--- vraptor-core/src/main/java/br/com/caelum/vraptor/jersey/VRaptorResourceConfig.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/jersey/VRaptorResourceConfig.java
@@ -1,10 +1,15 @@
 package br.com.caelum.vraptor.jersey;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.WebApplication;
 import com.sun.jersey.spi.container.servlet.WebComponent;
 import com.sun.jersey.spi.container.servlet.WebConfig;
 
@@ -20,5 +25,19 @@ public class VRaptorResourceConfig extends WebComponent {
 		ResourceConfig config = super.getDefaultResourceConfig(props, wc);
 		config.getSingletons().add(new FakeMethodDispatchProvider());
 		return config;
+	}
+
+	protected ContainerRequest createRequest(WebApplication app,
+			HttpServletRequest request, URI baseUri, URI requestUri)
+			throws IOException {
+		ContainerRequest container = super.createRequest(app, request, baseUri,
+				requestUri);
+		injectRequest(request, container);
+		return container;
+	}
+
+	private void injectRequest(HttpServletRequest request,
+			ContainerRequest container) {
+		container.getProperties().put(DefaultJersey.REQUEST, request);
 	}
 }

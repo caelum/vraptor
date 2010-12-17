4bdddfaf33fa5048f6e7c4fe5bde6839d29fcaf3
David Paniz, Jose Donizetti & Lucas Cavalcanti <lucasmrtuner@gmail.com>
2010-12-17 16:14:18 -0200

    new InterceptorRegistry

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/DefaultInterceptorRegistry.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/DefaultInterceptorRegistry.java
@@ -19,17 +19,9 @@ package br.com.caelum.vraptor.interceptor;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import br.com.caelum.vraptor.ioc.ApplicationScoped;
-import br.com.caelum.vraptor.ioc.Container;
-import br.com.caelum.vraptor.resource.ResourceMethod;
-import br.com.caelum.vraptor.util.collections.Filters;
-import br.com.caelum.vraptor.util.collections.Functions;
-
-import com.google.common.collect.Collections2;
-import com.google.common.collect.Lists;
 
 /**
  * A registry filled with interceptors to intercept requests.<br/>
@@ -43,12 +35,6 @@ import com.google.common.collect.Lists;
 public class DefaultInterceptorRegistry implements InterceptorRegistry {
 
     private final List<Class<? extends Interceptor>> interceptors = new ArrayList<Class<? extends Interceptor>>();
-
-    public List<Interceptor> interceptorsFor(ResourceMethod method, Container container) {
-		List<Interceptor> list = Lists.transform(interceptors, Functions.<Interceptor>instanceWith(container));
-		Collection<Interceptor> filtered = Collections2.filter(list, Filters.accepts(method));
-		return Lists.newArrayList(filtered);
-    }
 
     public void register(Class<? extends Interceptor>... interceptors) {
         this.interceptors.addAll(Arrays.asList(interceptors));
--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/InterceptorRegistry.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/InterceptorRegistry.java
@@ -19,16 +19,7 @@ package br.com.caelum.vraptor.interceptor;
 
 import java.util.List;
 
-import br.com.caelum.vraptor.ioc.Container;
-import br.com.caelum.vraptor.resource.ResourceMethod;
-
 public interface InterceptorRegistry {
-
-    /**
-     * Creates a list of interceptors which should intercept the required method
-     * for the specified container (request).
-     */
-    List<Interceptor> interceptorsFor(ResourceMethod method, Container container);
 
     void register(Class<? extends Interceptor> ... interceptors);
 

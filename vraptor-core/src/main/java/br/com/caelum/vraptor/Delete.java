616d44687dc69b2c300306e091ae73d8dbc76fd8
Davi <davisnog@gmail.com>
2011-01-30 15:40:43 -0200

    remove priority atribute

--- vraptor-core/src/main/java/br/com/caelum/vraptor/Delete.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/Delete.java
@@ -33,20 +33,4 @@ public @interface Delete {
 	 * @return
 	 */
     String[] value() default "";
-
-    /**
-     * Used to decide which path will be tested first.
-     * Paths with priority HIGHEST are tested before paths with priority HIGH,
-     * which are tested before paths with priority DEFAULT, and so on.
-     * <pre>
-	     @Path(value="/url", priority=Path.HIGHEST)
-	     @Path(value="/url", priority=Path.HIGH)
-	     @Path(value="/url", priority=Path.DEFAULT)
-	     @Path(value="/url", priority=Path.LOW)
-	     @Path(value="/url", priority=Path.LOWEST)
-     </pre>
-     *
-     */
-    int priority() default Path.DEFAULT;
-
 }

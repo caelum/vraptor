f6f4644f04c26deb05fe5a5f1af6ad6fed8138e8
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2011-02-18 09:59:18 -0200

    fixing corner case when bean definition has no type

--- vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/spring/StereotypedBeansRegistrar.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/spring/StereotypedBeansRegistrar.java
@@ -51,7 +51,11 @@ public class StereotypedBeansRegistrar implements ApplicationListener<Applicatio
 		String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
 		for (String name : beanDefinitionNames) {
 			Class<?> beanType = beanFactory.getType(name);
-			LOGGER.debug("scanning {}", beanType);
+			LOGGER.debug("scanning {} for bean definition {}", beanType, name);
+			if (beanType == null) {
+				LOGGER.info("null type for bean {}", name);
+				continue;
+			}
 
 			for (StereotypeHandler handler : stereotypeHandlers) {
 				LOGGER.trace("scanning {} with {}", beanType, handler);

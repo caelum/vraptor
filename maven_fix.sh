4facde517ce56080bfb2530afbfb0224a0fbf823
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2011-05-02 12:45:57 -0300

    upgrading iogi to 0.8.2

--- maven_fix.sh
+++ maven_fix.sh
@@ -4,7 +4,7 @@ mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-validato
 
 mvn install:install-file -DgroupId=org.hamcrest -DartifactId=hamcrest-all -Dversion=1.2RC3 -Dpackaging=jar -Dfile=vraptor-core/lib/optional/hamcrest-all-1.2RC3.jar
 
-mvn install:install-file -DgroupId=br.com.caelum -DartifactId=iogi -Dversion=0.8.1 -Dpackaging=jar -Dfile=vraptor-core/lib/optional/iogi/iogi-0.8.1.jar
+mvn install:install-file -DgroupId=br.com.caelum -DartifactId=iogi -Dversion=0.8.2 -Dpackaging=jar -Dfile=vraptor-core/lib/optional/iogi/iogi-0.8.2.jar
 
 mvn install:install-file -DgroupId=jersey -DartifactId=jersey-core -Dversion=1.5-SNAPSHOT -Dpackaging=jar -DgeneratePom=true -Dfile=vraptor-core/lib/jersey/jersey-core-1.5-SNAPSHOT.jar
 

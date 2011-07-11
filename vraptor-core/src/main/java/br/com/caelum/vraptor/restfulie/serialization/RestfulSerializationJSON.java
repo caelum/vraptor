2c9dc66f407491c59133e3a2126395c815b73274
Antonio Carlos da Graça Mota Durão de Souza <antonio.mota@corp.globo.com>
2011-07-10 23:24:08 -0300

    Convert one Relation to JSON.

---
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/restfulie/serialization/RestfulSerializationJSON.java
@@ -0,0 +1,62 @@
+/***
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource - guilherme.silveira@caelum.com.br
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.caelum.vraptor.restfulie.serialization;
+
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.caelum.vraptor.config.Configuration;
+import br.com.caelum.vraptor.interceptor.TypeNameExtractor;
+import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.ioc.RequestScoped;
+import br.com.caelum.vraptor.restfulie.Restfulie;
+import br.com.caelum.vraptor.serialization.ProxyInitializer;
+import br.com.caelum.vraptor.serialization.xstream.XStreamJSONSerialization;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.reflection.ReflectionConverter;
+
+/**
+ * Custom serialization process provides a way to add links to your resource representations.
+ * @author guilherme silveira
+ * @author ac de souza
+ */
+@Component
+@RequestScoped
+public class RestfulSerializationJSON extends XStreamJSONSerialization {
+
+	private final Restfulie restfulie;
+	private final Configuration config;
+
+	public RestfulSerializationJSON(HttpServletResponse response, TypeNameExtractor extractor, Restfulie restfulie, Configuration config, ProxyInitializer initializer) {
+		super(response,extractor,initializer);
+		this.restfulie = restfulie;
+		this.config = config;
+	}
+
+	/**
+	 * You can override this method for configuring XStream before serialization.
+	 * It configures the xstream instance with a link converter for all StateResource implementations.
+	 */
+	@Override
+	protected XStream getXStream() {
+		XStream xStream = super.getXStream();
+		MethodValueSupportConverter converter = new MethodValueSupportConverter(new ReflectionConverter(xStream.getMapper(), xStream.getReflectionProvider()));
+		xStream.registerConverter(new LinkConverterJSON(converter, restfulie, config));
+		return xStream;
+	}
+}
---
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/restfulie/LinkConverterJSONTest.java
@@ -0,0 +1,80 @@
+/***
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource - guilherme.silveira@caelum.com.br
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.caelum.vraptor.restfulie;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import br.com.caelum.vraptor.config.Configuration;
+import br.com.caelum.vraptor.restfulie.hypermedia.HypermediaResource;
+import br.com.caelum.vraptor.restfulie.relation.Relation;
+import br.com.caelum.vraptor.restfulie.relation.RelationBuilder;
+import br.com.caelum.vraptor.restfulie.serialization.LinkConverterJSON;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.reflection.ReflectionConverter;
+import com.thoughtworks.xstream.io.json.JsonHierarchicalStreamDriver;
+
+public class LinkConverterJSONTest {
+
+	private @Mock Restfulie restfulie;
+	private @Mock RelationBuilder builder;
+	private @Mock HypermediaResource resource;
+
+
+	private XStream xstream;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+
+		this.restfulie = mock(Restfulie.class);
+		when(restfulie.newRelationBuilder()).thenReturn(builder);
+
+		Configuration config = mock(Configuration.class);
+		when(config.getApplicationPath()).thenReturn("http://www.caelum.com.br");
+
+		xstream = new XStream(new JsonHierarchicalStreamDriver());
+		ReflectionConverter base = new ReflectionConverter(xstream.getMapper(), xstream.getReflectionProvider());
+
+		xstream.registerConverter(new LinkConverterJSON(base, restfulie, config));
+	}
+
+	@Test
+	public void shouldSerializeOneLinkIfThereIsATransition() {
+		Relation kill = mock(Relation.class);
+		when(kill.getName()).thenReturn("kill");
+		when(kill.getUri()).thenReturn("/kill");
+
+		when(builder.getRelations()).thenReturn(Arrays.asList(kill));
+		String xml = xstream.toXML(resource);
+		System.out.println(xml);
+		String expectedLinks = "\"links\": [\n    {\n      \"rel\": \"kill\",\n      \"href\": \"http://www.caelum.com.br/kill\"\n    }\n  ]";
+		System.out.println(expectedLinks);
+		assertThat(xml, containsString(expectedLinks));
+	}
+}

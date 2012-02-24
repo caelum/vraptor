c8b629f0e4e0d35a438ed2dea4506508f9dfd60f
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2012-02-24 18:31:00 -0200

    logging OGNL's workaround on GAE

--- vraptor-plugin-gae/src/br/com/caelum/vraptor/gae/AppEngineOgnlRuntime.java
+++ vraptor-plugin-gae/src/br/com/caelum/vraptor/gae/AppEngineOgnlRuntime.java
@@ -17,6 +17,10 @@
 package br.com.caelum.vraptor.gae;
 
 import ognl.OgnlRuntime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.caelum.vraptor.ioc.ApplicationScoped;
 import br.com.caelum.vraptor.ioc.Component;
 
@@ -26,7 +30,9 @@ import br.com.caelum.vraptor.ioc.Component;
 @Component
 @ApplicationScoped
 public class AppEngineOgnlRuntime {
+	private static Logger logger = LoggerFactory.getLogger(AppEngineOgnlRuntime.class);
     static {
+    	logger.info("Setting null on OGNL security manager, working around GAE");
     	OgnlRuntime.setSecurityManager(null);
     }
 }

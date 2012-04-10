a69a2b31cff80bcad9ae03234fcacea8badeb573
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2012-04-10 01:04:59 -0300

    fixing dependencies that were out of maven repo

--- maven_fix.sh
+++ maven_fix.sh
@@ -2,8 +2,4 @@
 
 mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-validator -Dversion=4.0.2.GA -Dclassifier=new -Dpackaging=jar -Dfile=vraptor-core/lib/optional/hibernate/hibernate-validator-4.0.2.GA.jar
 
-mvn install:install-file -DgroupId=org.hamcrest -DartifactId=hamcrest-all -Dversion=1.2RC3 -Dpackaging=jar -Dfile=vraptor-core/lib/optional/hamcrest-all-1.2RC3.jar
-
-mvn install:install-file -DgroupId=br.com.caelum -DartifactId=iogi -Dversion=0.8.2 -Dpackaging=jar -Dfile=vraptor-core/lib/optional/iogi/iogi-0.8.2.jar
-
 

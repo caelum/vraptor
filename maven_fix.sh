afbf99caa3c4942f9fd8b7f4fec805c4dbec0da3
Filipe Gomes Esperandio <filipesperandio@gmail.com>
2012-07-16 19:15:49 -0300

    fix hibernate dependency path

--- maven_fix.sh
+++ maven_fix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-validator -Dversion=4.0.2.GA -Dclassifier=new -Dpackaging=jar -Dfile=vraptor-core/lib/optional/hibernate/hibernate-validator-4.0.2.GA.jar
+mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-validator -Dversion=4.2.0.Final -Dclassifier=new -Dpackaging=jar -Dfile=vraptor-core/lib/optional/hibernate/hibernate-validator-4.2.0.Final.jar
 
 

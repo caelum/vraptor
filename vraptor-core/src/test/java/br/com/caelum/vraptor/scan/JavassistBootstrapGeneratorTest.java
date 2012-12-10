5635fa32b1b005d7e8718ad68f9faa48836bf72a
William Seiti Mizuta & Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2012-12-10 15:41:26 -0200

    Trying to fix broken build with classpath isolation

--- vraptor-core/src/test/java/br/com/caelum/vraptor/scan/JavassistBootstrapGeneratorTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/scan/JavassistBootstrapGeneratorTest.java
@@ -7,7 +7,9 @@ import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Collection;
+import java.util.concurrent.Callable;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -27,33 +29,54 @@ public class JavassistBootstrapGeneratorTest {
     	MockitoAnnotations.initMocks(this);
     	generator = new JavassistBootstrapGenerator();
     }
-    
+
     @Test
     public void shouldRegisterClasses() throws Exception {
-    	URL url = File.createTempFile("vraptor", "test").getParentFile().toURI().toURL();
-    	when(resolver.findWebInfClassesLocation()).thenReturn(url);
-    	
-    	Collection<String> components = asList(ResourceA.class.getName(), ResourceB.class.getName());
-		Class<WebAppBootstrap> generated = generator.generate(components, resolver);
-		WebAppBootstrap instance = generated.newInstance();
-		
-		instance.configure(registry);
-		
-		verify(registry).deepRegister(ResourceA.class);
-		verify(registry).deepRegister(ResourceB.class);
+    	runInADifferentClassLoader(new Callable<Void>() {
+    		public Void call() throws Exception {
+    			URL url = File.createTempFile("vraptor", "test").getParentFile().toURI().toURL();
+    			when(resolver.findWebInfClassesLocation()).thenReturn(url);
+
+    			Collection<String> components = asList(ResourceA.class.getName(), ResourceB.class.getName());
+    			Class<WebAppBootstrap> generated = generator.generate(components, resolver);
+    			WebAppBootstrap instance = generated.newInstance();
+
+    			instance.configure(registry);
+
+    			verify(registry).deepRegister(ResourceA.class);
+    			verify(registry).deepRegister(ResourceB.class);
+    			return null;
+    		}
+    	});
     }
-    
+
     @Test
     public void shouldThrowScannerExceptionIfAnErrorOccurs() throws Exception {
     	URL url = File.createTempFile("vraptor", "test").getParentFile().toURI().toURL();
     	when(resolver.findWebInfClassesLocation()).thenReturn(url);
-    	
+
     	try {
         	Collection<String> components = asList("a.resource.that.nobody.can.find");
     		generator.generate(components, resolver);
     		fail("Should throw ScannerException");
     	} catch (ScannerException e) {
-			
+
 		}
+    }
+
+    private void runInADifferentClassLoader(final Callable<?> c) throws Exception {
+    	ClassLoader classLoader = new URLClassLoader(new URL[] {});
+    	Thread t = new Thread(new Runnable() {
+    		public void run() {
+    			try {
+    				c.call();
+    			} catch (Exception e) {
+    				throw new RuntimeException(e);
+    			}
+    		}
+    	});
+    	t.setContextClassLoader(classLoader);
+    	t.start();
+    	t.join();
     }
 }

45b8bf06eaf90b114b7f8b348b573a254f90c424
Danilo Muñoz <munozdanilo@gmail.com>
2012-12-21 09:21:35 -0200

    Implement support to mock the http body from Result
    Applyed some corrections as proposed

--- vraptor-core/src/main/java/br/com/caelum/vraptor/util/test/MockHttpResult.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/util/test/MockHttpResult.java
@@ -1,4 +1,21 @@
-package br.com.multiway.interligation7.api.controller;
+/***
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.caelum.vraptor.util.test;
 
 import br.com.caelum.vraptor.View;
 import br.com.caelum.vraptor.proxy.CglibProxifier;
@@ -9,10 +26,20 @@ import br.com.caelum.vraptor.view.DefaultHttpResult;
 import br.com.caelum.vraptor.view.DefaultStatus;
 import br.com.caelum.vraptor.view.HttpResult;
 
-public class MockResultHttp extends MockResult{
+/**
+ *
+ * A mocked Result for testing the http body, content type and character encoding.
+ *
+ * It will store the http body when using result.use(Results.http()).addHeader("Content-Type", "application/json; charset=utf-8").body(json)
+ * and return this content as string.
+ *
+ * @author Danilo Muñoz
+ */
+@Component
+public class MockHttpResult extends MockResult{
 	private MockHttpServletResponse response = new MockHttpServletResponse();
 	
-	public MockResultHttp() {
+	public MockHttpResult() {
 		super(new CglibProxifier(new ObjenesisInstanceCreator()));
 	}
 	

33c6811644431917e6511f67b9ca8c5e263edcfe
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2013-04-03 21:30:23 -0300

    bumping version on java files

--- vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
@@ -100,7 +100,7 @@ public class VRaptor implements Filter {
 		servletContext = cfg.getServletContext();
 		BasicConfiguration config = new BasicConfiguration(servletContext);
 		init(config.getProvider());
-		logger.info("VRaptor 3.5.0-SNAPSHOT successfuly initialized");
+		logger.info("VRaptor 3.5.0 successfuly initialized");
 	}
 
 	void init(ContainerProvider provider) {
--- vraptor-core/src/main/java/br/com/caelum/vraptor/http/iogi/SetInstantiator.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/http/iogi/SetInstantiator.java
@@ -28,31 +28,31 @@ import br.com.caelum.iogi.reflection.Target;
 
 /**
  * An instantiator that supports Sets.
- * 
+ *
  * @author Otávio Scherer Garcia
  * @author Lucas Cavalcanti
- * @since 3.5.0-SNAPSHOT
+ * @since 3.5.0
  */
 public class SetInstantiator implements Instantiator<Set<Object>> {
-	
+
 	private ListInstantiator listInstantiator;
 
 	public SetInstantiator(Instantiator<Object> listElementInstantiator) {
 		listInstantiator = new ListInstantiator(listElementInstantiator);
-		
+
 	}
-	
+
 	public boolean isAbleToInstantiate(Target<?> target) {
 		return target.getClassType().isAssignableFrom(Set.class);
 	}
-	
+
 	public Set<Object> instantiate(Target<?> target, Parameters parameters) {
 		List<Object> list = listInstantiator.instantiate(target, parameters);
-		
+
 		if (list == null) {
 			return null;
 		}
-		
+
 		return new HashSet<Object>(list);
 	}
 }

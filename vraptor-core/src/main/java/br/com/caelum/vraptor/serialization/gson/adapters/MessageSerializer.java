c6677f2d3af1cc2a4eec232d7e40c9fccce5a042
Nykolas Lima <nykolas.lima@gmail.com>
2013-06-11 22:25:25 -0300

    fixing missed interface, @component and changing to use interface instead of concrete implementation

--- vraptor-core/src/main/java/br/com/caelum/vraptor/serialization/gson/adapters/MessageSerializer.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/serialization/gson/adapters/MessageSerializer.java
@@ -2,15 +2,20 @@ package br.com.caelum.vraptor.serialization.gson.adapters;
 
 import java.lang.reflect.Type;
 
-import br.com.caelum.vraptor.validator.I18nMessage;
+import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.ioc.RequestScoped;
+import br.com.caelum.vraptor.validator.Message;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
-public class MessageSerializer {
+@Component
+@RequestScoped
+public class MessageSerializer implements JsonSerializer<Message> {
 
-	public JsonElement serialize(I18nMessage message, Type type, JsonSerializationContext context) {
+	public JsonElement serialize(Message message, Type type, JsonSerializationContext context) {
 		String json = "{category:\"%s\",message:\"%s\"}";
 		
 		return new JsonParser().parse(String.format(json, message.getCategory(), message.getMessage()));

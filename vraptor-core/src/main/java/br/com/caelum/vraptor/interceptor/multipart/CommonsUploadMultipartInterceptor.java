d12949527c3e17d7300730b136f04076253647ff
Guilherme Gotardo <GGotardo@users.noreply.github.com>
2014-01-06 11:46:06 -0200

    Update CommonsUploadMultipartInterceptor.java

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/multipart/CommonsUploadMultipartInterceptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/multipart/CommonsUploadMultipartInterceptor.java
@@ -154,10 +154,10 @@ public class CommonsUploadMultipartInterceptor
         logger.warn("The file size limit was exceeded.", e);
     }
     
-		private void reportFileUploadException(FileUploadException e) {
-			validator.add(new ValidationMessage(e.getMessage(), "file.upload.exception"));
-			logger.warn("There was some problem parsing this multipart request, " + "or someone is not sending a RFC1867 compatible multipart request.", e);
-		}
+    private void reportFileUploadException(FileUploadException e) {
+	validator.add(new ValidationMessage(e.getMessage(), "file.upload.exception"));
+	logger.warn("There was some problem parsing this multipart request, " + "or someone is not sending a RFC1867 compatible multipart request.", e);
+    }
 
     protected void processFile(FileItem item, String name) {
         try {

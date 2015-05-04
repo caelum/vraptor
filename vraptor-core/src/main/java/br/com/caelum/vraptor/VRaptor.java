4d67f89a123fb9815809ba816f486f6ba34e4ff3
Rodrigo Turini <rdrg.turini@gmail.com>
2015-05-04 16:03:09 -0300

    updating filter version

--- vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
@@ -52,7 +52,7 @@ import br.com.caelum.vraptor.ioc.ContainerProvider;
  * @author Fabio Kung
  */
 public class VRaptor implements Filter {
-	private static final String VERSION = "3.5.4-SNAPSHOT";
+	private static final String VERSION = "3.5.5";
 	
 	private ContainerProvider provider;
 	private ServletContext servletContext;

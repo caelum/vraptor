f3bb6bb2dda330a0f2b4db2eaf4fba57071f07f0
Mohammed Ezzat <mohamed.ezzat@aurea.com>
2016-02-26 01:36:04 +0200

    squid:S106 - Standard outputs should not be used directly to log anything

--- vraptor-core/src/test/java/br/com/caelum/vraptor/view/SubGenericController.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/view/SubGenericController.java
@@ -1,20 +1,25 @@
 package br.com.caelum.vraptor.view;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Controller used to test Generic Controllers on LinkToHandler
  * @author Nykolas Lima
  *
  */
 public class SubGenericController extends GenericController<String> {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SubGenericController.class);
+
 	public void method(String string) {
-		System.out.println("Do something thing by Sub Generic Controller");
+		LOGGER.debug("Do something thing by Sub Generic Controller");
 	}
 	
 	public void okMethod(String string) {
-		System.out.println("OKOK");
+		LOGGER.debug("OKOK");
 	}
 	
 	public void anotherMethod(String string, String param) {
-		System.out.println("Do another thing by Sub Generic Controller");
+		LOGGER.debug("Do another thing by Sub Generic Controller");
 	}
 }
